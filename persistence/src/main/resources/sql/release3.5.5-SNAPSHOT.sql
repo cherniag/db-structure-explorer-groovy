2ea597fb2564a7a9e3609f67aa5ca99633b9a05b
Mykhailo Titov <mykhayl.titov@musicqubed.com>
2012-11-12 10:42:49 +0200

    IN PROGRESS - issue CL-7898: [JAdmin Server] Speed up media search http://jira.dev.now-technologies.mobi:8181/browse/CL-7898

--- persistence/src/main/resources/sql/release3.5.5-SNAPSHOT.sql
+++ persistence/src/main/resources/sql/release3.5.5-SNAPSHOT.sql
@@ -1,2 +1,5 @@
 -- Final insert of the release version
-insert into system (release_time_millis, version, release_name) values(unix_timestamp(now()), "3.5.5-SN", "Impetuous Zebra Omega");
+-- insert into system (release_time_millis, version, release_name) values(unix_timestamp(now()), "3.5.5-SN", "Impetuous Zebra Omega");
+
+alter table tb_mediaLog add index tb_mediaLog_PK_mediaUID (mediaUID), add constraint tb_mediaLog_U_mediaUID foreign key (mediaUID) references tb_media (i);
+alter table tb_drm add index tb_drm_PK_media (media), add constraint tb_drm_U_media foreign key (media) references tb_media (i);
