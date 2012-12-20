e52014669c2333928b4a04f80501dd31dc4fe61d
Alexandr Kolpakov <kalex@ciklum.com>
2012-12-20 15:13:20 +0200

    Creating O2ClientService

---
+++ service/src/main/java/mobi/nowtechnologies/server/service/O2ClientService.java
@@ -0,0 +1,7 @@
+package mobi.nowtechnologies.server.service;
+
+public interface O2ClientService {
+	
+	String validatePhoneNumber(String phoneNumber);
+
+}
---
+++ service/src/main/java/mobi/nowtechnologies/server/service/impl/O2ClientServiceImpl.java
@@ -0,0 +1,12 @@
+package mobi.nowtechnologies.server.service.impl;
+
+import mobi.nowtechnologies.server.service.O2ClientService;
+
+public class O2ClientServiceImpl implements O2ClientService{
+
+	@Override
+	public String validatePhoneNumber(String phoneNumber) {
+		return null;
+	}
+
+}
