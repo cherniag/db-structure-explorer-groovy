7d08762c69392497298ade58927a0fc37d10fc7e
Mykhailo Titov <mykhayl.titov@musicqubed.com>
2013-02-27 12:01:31 +0200

    API version changed

--- persistence/src/main/resources/sql/release3.8-SNAPSHOT.sql
+++ persistence/src/main/resources/sql/release3.8-SNAPSHOT.sql
@@ -1,2 +1,9 @@
  -- Final insert of the release version
 insert into system (release_time_millis, version, release_name) values(unix_timestamp(now()), "3.8-SN", "3.8-SN");
+
+-- CL-8742: [Server] Update CreatePendingPaymentJob to process o2 customers. Implementation of a new payment system
+-- http://jira.dev.now-technologies.mobi:8181/browse/CL-8742
+
+update tb_submittedpayments set next_sub_payment=0 where next_sub_payment is null;
+
+alter table tb_submittedpayments modify next_sub_payment int not null;
