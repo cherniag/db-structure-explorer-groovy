2040d000e325cdee1f9f030ebf46957b44d621b7
Mykhailo Titov <mykhayl.titov@musicqubed.com>
2013-02-27 16:26:06 +0200

    OPEN - issue CL-8742: [Server] Update CreatePendingPaymentJob to process o2 customers. Implementation of a new payment system http://jira.dev.now-technologies.mobi:8181/browse/CL-8742
    consumer, segment columns added into tb_users table

--- persistence/src/main/resources/sql/release3.8-SNAPSHOT.sql
+++ persistence/src/main/resources/sql/release3.8-SNAPSHOT.sql
@@ -7,3 +7,9 @@ insert into system (release_time_millis, version, release_name) values(unix_time
 update tb_submittedpayments set next_sub_payment=0 where next_sub_payment is null;
 
 alter table tb_submittedpayments modify next_sub_payment int not null;
+
+-- CL-8742: [Server] Update CreatePendingPaymentJob to process o2 customers. Implementation of a new payment system
+-- http://jira.dev.now-technologies.mobi:8181/browse/CL-8742
+
+alter table tb_users add column consumer varchar(255);
+alter table tb_users add column segment varchar(255);
