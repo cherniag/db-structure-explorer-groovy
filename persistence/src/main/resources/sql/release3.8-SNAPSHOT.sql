b765414fcd0dc166b84e19a72790f8697d92be86
Mykhailo Titov <mykhayl.titov@musicqubed.com>
2013-02-27 16:54:12 +0200

    OPEN - issue CL-8742: [Server] Update CreatePendingPaymentJob to process o2 customers. Implementation of a new payment system http://jira.dev.now-technologies.mobi:8181/browse/CL-8742
    bug fixed: consumer column renamed on contract

--- persistence/src/main/resources/sql/release3.8-SNAPSHOT.sql
+++ persistence/src/main/resources/sql/release3.8-SNAPSHOT.sql
@@ -11,5 +11,5 @@ alter table tb_submittedpayments modify next_sub_payment int not null;
 -- CL-8742: [Server] Update CreatePendingPaymentJob to process o2 customers. Implementation of a new payment system
 -- http://jira.dev.now-technologies.mobi:8181/browse/CL-8742
 
-alter table tb_users add column consumer varchar(255);
+alter table tb_users add column contract varchar(255);
 alter table tb_users add column segment varchar(255);
