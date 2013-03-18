39ec70ad48fe1f1c06f8f65669a13429b56ceee6
Mykhailo Titov <mykhayl.titov@musicqubed.com>
2013-03-18 16:37:19 +0200

    User toString() method changed

--- persistence/src/main/java/mobi/nowtechnologies/server/persistence/domain/User.java
+++ persistence/src/main/java/mobi/nowtechnologies/server/persistence/domain/User.java
@@ -1195,6 +1195,7 @@ public class User implements Serializable {
                 .add("deviceString", deviceString)
                 .add("freeTrialStartedTimestampMillis", freeTrialStartedTimestampMillis)
                 .add("activationStatus", activationStatus)
+                .add("segment", segment)
                 .add("provider", provider)
                 .add("contract", contract).toString();
     }
