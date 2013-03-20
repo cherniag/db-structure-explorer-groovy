662c48cf71cc5a335695a05efe0938517bede7f3
Mykhailo Titov <mykhayl.titov@musicqubed.com>
2013-03-20 20:01:49 +0200

    bug with release3.8-SNAPSHOT.sql fixed

--- persistence/src/main/resources/sql/release3.8-SNAPSHOT.sql
+++ persistence/src/main/resources/sql/release3.8-SNAPSHOT.sql
@@ -4,42 +4,25 @@ insert into system (release_time_millis, version, release_name) values(unix_time
 -- CL-8742: [Server] Update CreatePendingPaymentJob to process o2 customers. Implementation of a new payment system
 -- http://jira.dev.now-technologies.mobi:8181/browse/CL-8742
 
-update tb_submittedpayments set next_sub_payment=0 where next_sub_payment is null;
+update tb_submittedPayments set next_sub_payment=0 where next_sub_payment is null;
 
-alter table tb_submittedpayments modify next_sub_payment int not null;
+alter table tb_submittedPayments modify next_sub_payment int not null;
 
 -- CL-8742: [Server] Update CreatePendingPaymentJob to process o2 customers. Implementation of a new payment system
 -- http://jira.dev.now-technologies.mobi:8181/browse/CL-8742
 
-alter table tb_users add column contract char(255);
+-- Verify on prod  alter table tb_users add column contract char(255);
 alter table tb_users add column segment char(255);
 alter table tb_users add column last_payment_try_in_cycle_millis BIGINT default 0;
 alter table tb_users add column deactivated_grace_credit_millis BIGINT default 0;
 
-alter table tb_paymentdetails add column phone_number varchar(255);
+alter table tb_paymentDetails add column phone_number varchar(255);
 alter table tb_paymentPolicy add column contract char(255);
 alter table tb_paymentPolicy add column segment char(255);
 alter table tb_paymentPolicy add column content_category varchar(255);
 alter table tb_paymentPolicy add column content_type varchar(255);
 alter table tb_paymentPolicy add column content_description varchar(255);
 alter table tb_paymentPolicy add column sub_merchant_id varchar(255);
-
-create table grace_period
-(
-   id int not null auto_increment,
---   community_id tinyint not null,
-   user_group_id tinyint not null,
-   contract char(255),
-   segment char(255),
-   provider varchar(255),
-   duration_millis bigint not null, 
-   primary key (`id`),
--- unique (contract, community_id, segment, provider)
-   unique (contract, user_group_id, segment, provider)
-)
-engine=innodb default charset=utf8;
-
-create unique index PRIMARY on grace_period(id);
 
 -- Before 48 sms
 alter table tb_users add column last_before48_sms_millis BIGINT default 0;
