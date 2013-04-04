a7fb5ac1e6821f23e19b95fb622f56b2bfd4ac9b
Volodymyr Derkach <vlde@ciklum.com>
2013-04-04 14:39:33 +0300

    revert merge accounts

--- service/src/main/java/mobi/nowtechnologies/server/service/UserService.java
+++ service/src/main/java/mobi/nowtechnologies/server/service/UserService.java
@@ -1603,7 +1603,7 @@ public class UserService {
 		Community community = communityService.getCommunityByName(userDeviceRegDetailsDto.getCommunityName());
 		User user = findByDeviceUIDAndCommunityRedirectURL(deviceUID, community.getRewriteUrlParameter());
 
-		if (null == user || user.getActivationStatus() == ActivationStatus.ACTIVATED) {
+		if (null == user) {
 			user = createUser(userDeviceRegDetailsDto, deviceUID, deviceType, community);
 		}
 
