314ae93d473bee39fa426d3cd892de599a57a640
Alexandr Kolpakov <kalex@ciklum.com>
2013-04-23 12:01:20 +0300

    fix release3.9.sql script

--- persistence/src/main/resources/sql/release3.9.sql
+++ persistence/src/main/resources/sql/release3.9.sql
@@ -3,3 +3,6 @@ insert into system (release_time_millis, version, release_name) values(unix_time
 
 -- IMP-1263 [BILLING] Track users changing segment and unsubscribe them
 alter table tb_paymentPolicy add column provider char(255);
+
+update tb_paymentPolicy pp set pp.provider = 'non-o2' where pp.segment is null and pp.communityID = 10
+update tb_paymentPolicy pp set pp.provider = 'o2' where pp.segment is not null and pp.communityID = 10
