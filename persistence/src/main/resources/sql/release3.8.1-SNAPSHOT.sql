88bfa2e8240b6fcf1b73b5b30046d825b4c891ca
Mykhailo Titov <mykhayl.titov@musicqubed.com>
2013-05-10 11:46:04 +0300

    Implement a badge on IOS Home Screen when new App content available http://jira.musicqubed.com/browse/IMP-1385

--- persistence/src/main/resources/sql/release3.8.1-SNAPSHOT.sql
+++ persistence/src/main/resources/sql/release3.8.1-SNAPSHOT.sql
@@ -1,5 +1,5 @@
  -- Final insert of the release version
-insert into system (release_time_millis, version, release_name) values(unix_timestamp(now()), "3.9-SN", "3.9-SN");
+insert into system (release_time_millis, version, release_name) values(unix_timestamp(now()), "3.8.1-SN", "3.8.1-SN");
 
 -- Implement a badge on IOS Home Screen when new App content available
 -- http://jira.musicqubed.com/browse/IMP-1385
