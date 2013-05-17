993e76fcabbbc0ea9af53818d6fda3e5718d980e
Mykhailo Titov <mykhayl.titov@musicqubed.com>
2013-05-17 11:37:31 +0300

    Improve logging in the Transport API calls
    http://jira.musicqubed.com/browse/PRO-2154
    
    profile logic added into MOListener command was changed

--- transport/src/main/java/mobi/nowtechnologies/server/transport/controller/MigController.java
+++ transport/src/main/java/mobi/nowtechnologies/server/transport/controller/MigController.java
@@ -1,5 +1,6 @@
 package mobi.nowtechnologies.server.transport.controller;
 
+import mobi.nowtechnologies.server.persistence.domain.PaymentDetails;
 import mobi.nowtechnologies.server.persistence.domain.SubmittedPayment;
 import mobi.nowtechnologies.server.persistence.domain.User;
 import mobi.nowtechnologies.server.service.UserService;
@@ -17,6 +18,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import java.net.URLDecoder;
+import java.util.List;
 
 /**
  * MigController
@@ -25,44 +27,44 @@ import java.net.URLDecoder;
  * 
  */
 @Controller
-public class MigController extends ProfileController{
+public class MigController extends ProfileController {
 	private static final Logger LOGGER = LoggerFactory.getLogger(MigController.class);
-	
+
 	private static final String STOP = "Stop";
 
 	private UserService userService;
 	private MigPaymentService migPaymentService;
-	
+
 	public void setUserService(UserService userService) {
 		this.userService = userService;
 	}
-	
+
 	public void setMigPaymentService(MigPaymentService migPaymentService) {
 		this.migPaymentService = migPaymentService;
 	}
 
 	@RequestMapping(method = RequestMethod.GET, value = "/DRListener")
-	public void callback(@RequestParam (value = "MESSAGEID") String messageId,
-			@RequestParam (value = "STATUSTYPE") String statusType,
-			@RequestParam (value = "GUID") String guid,
-			@RequestParam (value = "STATUS") String status,
-			@RequestParam (value = "DESCRIPTION", required=false) String description,
+	public void callback(@RequestParam(value = "MESSAGEID") String messageId,
+			@RequestParam(value = "STATUSTYPE") String statusType,
+			@RequestParam(value = "GUID") String guid,
+			@RequestParam(value = "STATUS") String status,
+			@RequestParam(value = "DESCRIPTION", required = false) String description,
 			HttpServletResponse response,
 			HttpServletRequest request) {
-		LOGGER.info("[START] MIG query string is [{}]",request.getQueryString());
-		LOGGER.info("DRListener command processing started. MESSAGEID=[{}], STATUSTYPE=[{}], GUID=[{}], STATUS=[{}]", new String[] {messageId, statusType, guid, status });
+		LOGGER.info("[START] MIG query string is [{}]", request.getQueryString());
+		LOGGER.info("DRListener command processing started. MESSAGEID=[{}], STATUSTYPE=[{}], GUID=[{}], STATUS=[{}]", new String[] { messageId, statusType, guid, status });
 		User user = null;
 		boolean isFailed = false;
 		try {
 			if (messageId == null)
 				throw new NullPointerException("The parameter messageId is null");
-			
+
 			String decodeDescription = "";
 			if (StringUtils.hasText(description))
 				decodeDescription = URLDecoder.decode(description, "UTF-8");
-			
+
 			SubmittedPayment submittedPayment = migPaymentService.commitPayment(messageId, status, decodeDescription);
-			if(submittedPayment!=null){
+			if (submittedPayment != null) {
 				user = submittedPayment.getUser();
 			}
 		} catch (Exception e) {
@@ -76,25 +78,36 @@ public class MigController extends ProfileController{
 			LOGGER.info("[DONE] invoking DRListener command");
 		}
 	}
-	
+
 	@RequestMapping(method = RequestMethod.GET, value = "/MOListener")
-	public void stopService(@RequestParam (value = "BODY") String action,
-			@RequestParam (value = "OADC") String mobile,
-			@RequestParam (value = "CONNECTION") String operatorMigName,
+	public void stopService(@RequestParam(value = "BODY") String action,
+			@RequestParam(value = "OADC") String mobile,
+			@RequestParam(value = "CONNECTION") String operatorMigName,
 			HttpServletResponse response,
 			HttpServletRequest request) {
-		//Only 1 case when we get this request from MIG - stop service
+		// Only 1 case when we get this request from MIG - stop service
 		LOGGER.info("[START] MOLISTENER command processing started");
+		User user = null;
+		boolean isFailed = false;
 		try {
-			if (STOP.equalsIgnoreCase(action))
-				userService.unsubscribeUser(mobile, operatorMigName);
-			else
+			if (STOP.equalsIgnoreCase(action)) {
+				List<PaymentDetails> paymentDetails = userService.unsubscribeUser(mobile, operatorMigName);
+				if (paymentDetails != null && !paymentDetails.isEmpty() && paymentDetails.get(0) != null) {
+					user = paymentDetails.get(0).getOwner();
+				}
+			} else {
 				throw new IllegalStateException("action [" + action + "] not supported");
+			}
 		} catch (Exception e) {
+			isFailed = true;
+			logProfileData(null, null, null, user, e);
 			LOGGER.error("error processing MOLISTENER command", e);
 		} finally {
+			if (!isFailed) {
+				logProfileData(null, null, null, user, null);
+			}
 			LOGGER.info("[DONE] invoking MOListener command");
 		}
 	}
-	
+
 }
