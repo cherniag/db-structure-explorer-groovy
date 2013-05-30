432c2fd208ab6d1f1c9f0e4f18f86ebad13ad14f
Alexandr Kolpakov <kalex@ciklum.com>
2013-05-30 11:22:55 +0300

    fix some controller

--- web/src/main/java/mobi/nowtechnologies/server/web/controller/PlaylistController.java
+++ web/src/main/java/mobi/nowtechnologies/server/web/controller/PlaylistController.java
@@ -1,25 +1,18 @@
 package mobi.nowtechnologies.server.web.controller;
 
 import java.io.IOException;
-import java.util.*;
+import java.util.Date;
+import java.util.List;
 
-import mobi.nowtechnologies.server.persistence.domain.Chart;
 import mobi.nowtechnologies.server.persistence.domain.ChartDetail;
-import mobi.nowtechnologies.server.persistence.domain.User;
-import mobi.nowtechnologies.server.persistence.repository.ChartRepository;
 import mobi.nowtechnologies.server.service.ChartDetailService;
 import mobi.nowtechnologies.server.service.ChartService;
-import mobi.nowtechnologies.server.service.UserService;
 import mobi.nowtechnologies.server.shared.enums.ChartType;
 import mobi.nowtechnologies.server.shared.web.filter.CommunityResolverFilter;
 import mobi.nowtechnologies.server.web.dtos.PlaylistDto;
-
 import mobi.nowtechnologies.server.web.dtos.TrackDto;
-import org.codehaus.jackson.JsonGenerationException;
-import org.codehaus.jackson.map.ObjectMapper;
+
 import org.springframework.stereotype.Controller;
-import org.springframework.transaction.annotation.Propagation;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -27,34 +20,16 @@ import org.springframework.web.servlet.ModelAndView;
 public class PlaylistController extends CommonController {
 	
 	public static final String VIEW_PLAYLIST = "playlist";
-	public static final String VIEW_PLAYLIST_TRACKS = "playlist/tracks";
-	
 	public static final String PAGE_PLAYLIST = VIEW_PLAYLIST+"/{playlistType}";
-	public static final String PAGE_PLAYLIST_TRACKS = "playlist/{playlistId}/tracks";
 	public static final String REDIREC_PAGE_SWAP = "redirect:/playlist/swap.html";
 	
-	private UserService userService;
     private ChartDetailService chartDetailService;
 	private ChartService chartService;
     private String urlToCDN;
 	
 	@RequestMapping(value=PAGE_PLAYLIST, method=RequestMethod.GET)
 	public ModelAndView getPlaylistPage(@PathVariable("playlistType")ChartType playlistType, @CookieValue(value = CommunityResolverFilter.DEFAULT_COMMUNITY_COOKIE_NAME) String communityURL) {
-		List<ChartDetail> playlists = chartService.getChartsByCommunity(communityURL, null, playlistType);
-		
-		//TODO get temparally one playlist for direct select
-		User user = userService.getUserWithSelectedCharts(getSecurityContextDetails().getUserId());
-		List<PlaylistDto> playlistDtos = new ArrayList<PlaylistDto>();
-		for (ChartDetail chartDetail : playlists) {
-			if((!chartDetail.getDefaultChart() && user.getSelectedCharts().size() == 0)
-					|| (user.getSelectedCharts().size() > 0 && !user.getSelectedCharts().get(0).getI().equals(chartDetail.getChartId()))){
-				playlistDtos.add(new PlaylistDto(chartDetail));
-				break;
-			}	
-		}
-
 		ModelAndView modelAndView = new ModelAndView(VIEW_PLAYLIST);
-		modelAndView.addObject(PlaylistDto.NAME_LIST, playlistDtos);
 			
 		return modelAndView;
 	}
@@ -65,14 +40,14 @@ public class PlaylistController extends CommonController {
         return new ModelAndView().addObject("playlists", PlaylistDto.toList(charts));
     }
 
-    @RequestMapping(value="playlists/{playlistID}", produces = "application/json", method=RequestMethod.GET)
-    public ModelAndView getTracks(@PathVariable("playlistID")Byte playlistID){
+    @RequestMapping(value="playlists/{playlistId}", produces = "application/json", method=RequestMethod.GET)
+    public ModelAndView getTracks(@PathVariable("playlistId")Byte playlistID){
         List<ChartDetail> chartDetails = chartDetailService.getChartItemsByDate(playlistID, new Date(), false);
         List<TrackDto> tracks = TrackDto.toList(chartDetails, urlToCDN);
         return new ModelAndView().addObject("tracks", tracks);
     }
 	
-	@RequestMapping(value=PAGE_PLAYLIST_TRACKS, method=RequestMethod.POST)
+	@RequestMapping(value="playlists/{playlistId}", method=RequestMethod.POST)
 	public ModelAndView selectPlaylists(@PathVariable("playlistId") Integer playlistId,
 			@CookieValue(value = CommunityResolverFilter.DEFAULT_COMMUNITY_COOKIE_NAME) String communityURL) {
 		
@@ -85,9 +60,6 @@ public class PlaylistController extends CommonController {
 		this.chartService = chartService;
 	}
 
-	public void setUserService(UserService userService) {
-		this.userService = userService;
-	}
 
     public void setChartDetailService(ChartDetailService chartDetailService) {
         this.chartDetailService = chartDetailService;
