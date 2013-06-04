1ee748d8420cb0f01de15a0d09d2315d65e9953e
Mykhailo Titov <mykhayl.titov@musicqubed.com>
2013-06-04 16:29:45 +0300

    [Jadmin] Add rich popup which allows deeplinking to various "anchors" in the app/mobile web
    http://jira.musicqubed.com/browse/IMP-1542
    
    validation logic added into MessageDtoValidator

--- admin/src/main/java/mobi/nowtechnologies/server/admin/validator/MessageDtoValidator.java
+++ admin/src/main/java/mobi/nowtechnologies/server/admin/validator/MessageDtoValidator.java
@@ -1,26 +1,48 @@
 package mobi.nowtechnologies.server.admin.validator;
 
 import mobi.nowtechnologies.server.service.util.BaseValidator;
+import mobi.nowtechnologies.server.shared.dto.NewsDetailDto.MessageType;
 import mobi.nowtechnologies.server.shared.dto.admin.MessageDto;
+import mobi.nowtechnologies.server.shared.enums.MessageActionType;
+
+import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.validation.Errors;
 
 public class MessageDtoValidator extends BaseValidator {
-	
+
 	private static final Logger LOGGER = LoggerFactory.getLogger(MessageDtoValidator.class);
 
 	@Override
 	public boolean customValidate(Object target, Errors errors) {
 		LOGGER.debug("input parameters target, errors: [{}], [{}]", target, errors);
-		
-//		MessageDto messageDto = (MessageDto) target;
-//		
-//		if (messageDto.getFile().isEmpty())
-//			errors.rejectValue("file", "message.file.isEmpty", "No file has been selected or file has no content");
-		
+
+		MessageDto messageDto = (MessageDto) target;
+
+		if (messageDto.getMessageType().equals(MessageType.RICH_POPUP)) {
+
+			String action = messageDto.getAction();
+			MessageActionType messageActionType = messageDto.getActionType();
+			if (StringUtils.isBlank(action)) {
+				if (messageActionType.equals(MessageActionType.A_SPECIFIC_NEWS_STORY) || messageActionType.equals(MessageActionType.A_SPECIFIC_TRACK)
+						|| messageActionType.equals(MessageActionType.EXTERNAL_URL) || messageActionType.equals(MessageActionType.MOBILE_WEB_PORTAL)) {
+					errors.rejectValue("action", "richPopups.action.isEmptyOrBlank", "The action field couldn't be empty or blank for this action type");
+				}
+			} else if (action.length() > 255) {
+				errors.rejectValue("action", "richPopups.action.size", "This is field must consist of 1-255 characters");
+			}
+
+			final String actionButtonText = messageDto.getActionButtonText();
+			if (StringUtils.isBlank(actionButtonText)) {
+				errors.rejectValue("actionButtonText", "richPopups.actionButtonText.isEmptyOrBlank", "The action button text field couldn't be empty or blank");
+			}else if (actionButtonText.length() > 255) {
+				errors.rejectValue("action", "richPopups.actionButtonText.size", "This is field must consist of 1-255 characters");
+			}
+		}
+
 		boolean hasErrors = errors.hasErrors();
-		
+
 		LOGGER.debug("Output parameter hasErrors=[{}]", hasErrors);
 		return hasErrors;
 	}
