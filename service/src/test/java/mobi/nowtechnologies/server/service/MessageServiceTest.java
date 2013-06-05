a7eabb4bf263103d36336c1074ea60b41359904d
Mykhailo Titov <mykhayl.titov@musicqubed.com>
2013-06-05 13:29:46 +0300

    [Jadmin] Add rich popup which allows deeplinking to various "anchors" in the app/mobile web
    http://jira.musicqubed.com/browse/IMP-1542
    
    tests added

--- service/src/test/java/mobi/nowtechnologies/server/service/MessageServiceTest.java
+++ service/src/test/java/mobi/nowtechnologies/server/service/MessageServiceTest.java
@@ -2,14 +2,27 @@ package mobi.nowtechnologies.server.service;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.Date;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+import mobi.nowtechnologies.server.factory.admin.FilterDtoFactory;
+import mobi.nowtechnologies.server.persistence.dao.CommunityDao;
 import mobi.nowtechnologies.server.persistence.domain.AbstractFilterWithCtiteria;
 import mobi.nowtechnologies.server.persistence.domain.Community;
 import mobi.nowtechnologies.server.persistence.domain.CommunityFactory;
@@ -19,17 +32,16 @@ import mobi.nowtechnologies.server.persistence.repository.MessageRepository;
 import mobi.nowtechnologies.server.shared.Utils;
 import mobi.nowtechnologies.server.shared.dto.NewsDetailDto.MessageType;
 import mobi.nowtechnologies.server.shared.dto.admin.FilterDto;
+import mobi.nowtechnologies.server.shared.dto.admin.MessageDto;
+import mobi.nowtechnologies.server.shared.dto.admin.MessageDtoFactory;
 
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
 import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -38,10 +50,10 @@ import org.springframework.web.multipart.MultipartFile;
  * 
  */
 @RunWith(PowerMockRunner.class)
-@PrepareForTest(Utils.class)
+@PrepareForTest({Utils.class, Message.class})
 public class MessageServiceTest {
 
-	private MessageService fixture;
+	private MessageService messageServiceSpy;
 	private MessageRepository mockMessageRepository;
 	private CommunityService mockCommunityService;
 	private FilterService mockFilterService;
@@ -58,7 +70,7 @@ public class MessageServiceTest {
 
 		Mockito.doNothing().when(mockMessageRepository).delete(message);
 
-		fixture.delete(messageId);
+		messageServiceSpy.delete(messageId);
 
 		assertNotNull(message.getFilterWithCtiteria());
 		assertEquals(Collections.<AbstractFilterWithCtiteria> emptySet(), message.getFilterWithCtiteria());
@@ -81,7 +93,7 @@ public class MessageServiceTest {
 		Mockito.when(mockMessageRepository.findByCommunityAndMessageTypesAndPublishTimeMillis(Mockito.any(Community.class), Mockito.anyListOf(MessageType.class), Mockito.anyLong())).thenReturn(null);
 		Mockito.when(mockMessageRepository.findByCommunityAndMessageTypes(Mockito.any(Community.class), Mockito.anyListOf(MessageType.class))).thenReturn(messageList);
 
-		List<Message> result = fixture.getAds(communityURL);
+		List<Message> result = messageServiceSpy.getAds(communityURL);
 
 		assertNotNull(result);
 
@@ -115,7 +127,7 @@ public class MessageServiceTest {
 		Mockito.when(mockMessageRepository.save(message)).thenReturn(message);
 		Mockito.when(mockCloudFileService.uploadFile(multipartFile, imageFileName)).thenReturn(Boolean.TRUE);
 
-		Message result = fixture.saveAd(message, multipartFile, communityURL, filterDtos, removeImage);
+		Message result = messageServiceSpy.saveAd(message, multipartFile, communityURL, filterDtos, removeImage);
 
 		assertNotNull(result);
 		assertEquals(message, result);
@@ -150,7 +162,7 @@ public class MessageServiceTest {
 		Mockito.when(mockMessageRepository.save(message)).thenReturn(message);
 		Mockito.when(mockCloudFileService.uploadFile(multipartFile, imageFileName)).thenReturn(Boolean.TRUE);
 
-		Message result = fixture.saveAd(message, multipartFile, communityURL, filterDtos, removeImage);
+		Message result = messageServiceSpy.saveAd(message, multipartFile, communityURL, filterDtos, removeImage);
 
 		assertNotNull(result);
 		assertEquals(message, result);
@@ -185,7 +197,7 @@ public class MessageServiceTest {
 		Mockito.when(mockMessageRepository.save(message)).thenReturn(message);
 		Mockito.when(mockCloudFileService.uploadFile(multipartFile, imageFileName)).thenReturn(Boolean.TRUE);
 
-		Message result = fixture.saveAd(message, multipartFile, communityURL, filterDtos, removeImage);
+		Message result = messageServiceSpy.saveAd(message, multipartFile, communityURL, filterDtos, removeImage);
 
 		assertNotNull(result);
 		assertEquals(message, result);
@@ -216,7 +228,7 @@ public class MessageServiceTest {
 		Mockito.when(mockMessageRepository.save(message)).thenReturn(message);
 		Mockito.when(mockCloudFileService.uploadFile(multipartFile, message.getImageFileName())).thenReturn(Boolean.TRUE);
 
-		Message result = fixture.updateAd(message, multipartFile, communityURL, filterDtos, removeImage);
+		Message result = messageServiceSpy.updateAd(message, multipartFile, communityURL, filterDtos, removeImage);
 		
 		assertNotNull(result);
 		assertEquals(message, result);
@@ -245,7 +257,7 @@ public class MessageServiceTest {
 		Mockito.when(mockMessageRepository.save(message)).thenReturn(message);
 		Mockito.when(mockCloudFileService.uploadFile(multipartFile, message.getImageFileName())).thenReturn(Boolean.TRUE);
 
-		Message result = fixture.updateAd(message, multipartFile, communityURL, filterDtos, removeImage);
+		Message result = messageServiceSpy.updateAd(message, multipartFile, communityURL, filterDtos, removeImage);
 		
 		assertNotNull(result);
 		assertEquals(message, result);
@@ -276,7 +288,7 @@ public class MessageServiceTest {
 		Mockito.when(mockMessageRepository.save(message)).thenReturn(message);
 		Mockito.when(mockCloudFileService.uploadFile(multipartFile, message.getImageFileName())).thenReturn(Boolean.TRUE);
 
-		Message result = fixture.updateAd(message, multipartFile, communityURL, filterDtos, removeImage);
+		Message result = messageServiceSpy.updateAd(message, multipartFile, communityURL, filterDtos, removeImage);
 		
 		assertNotNull(result);
 		assertEquals(message, result);
@@ -307,7 +319,7 @@ public class MessageServiceTest {
 		Mockito.when(mockMessageRepository.save(message)).thenReturn(message);
 		Mockito.when(mockCloudFileService.uploadFile(multipartFile, message.getImageFileName())).thenReturn(Boolean.TRUE);
 
-		Message result = fixture.updateAd(message, multipartFile, communityURL, filterDtos, removeImage);
+		Message result = messageServiceSpy.updateAd(message, multipartFile, communityURL, filterDtos, removeImage);
 		
 		assertNotNull(result);
 		assertEquals(message, result);
@@ -337,7 +349,7 @@ public class MessageServiceTest {
 		Mockito.when(mockMessageRepository.save(message)).thenReturn(message);
 		Mockito.when(mockCloudFileService.uploadFile(multipartFile, message.getImageFileName())).thenReturn(Boolean.TRUE);
 
-		Message result = fixture.updateAd(message, multipartFile, communityURL, filterDtos, removeImage);
+		Message result = messageServiceSpy.updateAd(message, multipartFile, communityURL, filterDtos, removeImage);
 		
 		assertNotNull(result);
 		assertEquals(message, result);
@@ -349,6 +361,157 @@ public class MessageServiceTest {
 		Mockito.verify(mockCloudFileService, Mockito.times(0)).uploadFile(multipartFile, message.getImageFileName());
 	}
 	
+	@Test
+	public void testSave_Success() throws Exception {
+		
+		MessageDto messageDto = MessageDtoFactory.createMessageDto();
+		String communityURL = "o2";
+		
+		Message message = MessageFactory.createMessage(null);
+				
+		doReturn(message).when(messageServiceSpy).saveOrUpdate(eq(messageDto), eq(communityURL), any(Message.class));
+		
+		Message actualMessage = messageServiceSpy.save(messageDto, communityURL);
+		
+		assertNotNull(actualMessage);
+		assertEquals(message, actualMessage);
+		
+		verify(messageServiceSpy, times(1)).saveOrUpdate(eq(messageDto), eq(communityURL), any(Message.class));
+	}
+	
+	@Test(expected=Exception.class)
+	public void testSave_Failure() throws Exception {
+		
+		MessageDto messageDto = MessageDtoFactory.createMessageDto();
+		String communityURL = "o2";
+				
+		doThrow(new Exception()).when(messageServiceSpy).saveOrUpdate(eq(messageDto), eq(communityURL), any(Message.class));
+		
+		messageServiceSpy.save(messageDto, communityURL);
+	}
+	
+	@Test
+	public void testUpdate_Success() throws Exception {
+		
+		MessageDto messageDto = MessageDtoFactory.createMessageDto();
+		messageDto.setId(Integer.MAX_VALUE);
+		
+		String communityURL = "o2";
+		
+		Message message = MessageFactory.createMessage(null);
+		
+		when(mockMessageRepository.findOne(messageDto.getId())).thenReturn(message);
+		
+		doReturn(message).when(messageServiceSpy).saveOrUpdate(eq(messageDto), eq(communityURL), any(Message.class));
+		
+		Message actualMessage = messageServiceSpy.update(messageDto, communityURL);
+		
+		assertNotNull(actualMessage);
+		assertEquals(message, actualMessage);
+		
+		verify(messageServiceSpy, times(1)).saveOrUpdate(eq(messageDto), eq(communityURL), any(Message.class));
+	}
+	
+	@Test
+	public void testUpdate_NoMessage_Success() throws Exception {
+		
+		MessageDto messageDto = MessageDtoFactory.createMessageDto();
+		messageDto.setId(Integer.MAX_VALUE);
+		
+		String communityURL = "o2";
+		
+		Message message = MessageFactory.createMessage(null);
+		
+		when(mockMessageRepository.findOne(messageDto.getId())).thenReturn(null);
+		
+		doReturn(message).when(messageServiceSpy).saveOrUpdate(eq(messageDto), eq(communityURL), any(Message.class));
+		
+		Message actualMessage = messageServiceSpy.update(messageDto, communityURL);
+		
+		assertNull(actualMessage);
+		
+		verify(messageServiceSpy, times(0)).saveOrUpdate(eq(messageDto), eq(communityURL), any(Message.class));
+	}
+	
+	@Test(expected=Exception.class)
+	public void testUpdate_saveOrUpdateThrowsException_Failure() throws Exception {
+		
+		MessageDto messageDto = MessageDtoFactory.createMessageDto();
+		messageDto.setId(Integer.MAX_VALUE);
+		
+		String communityURL = "o2";
+		
+		Message message = MessageFactory.createMessage(null);
+		
+		when(mockMessageRepository.findOne(messageDto.getId())).thenReturn(message);
+		
+		doThrow(new Exception()).when(messageServiceSpy).saveOrUpdate(eq(messageDto), eq(communityURL), any(Message.class));
+		
+		messageServiceSpy.update(messageDto, communityURL);
+	}
+	
+//	@Test
+//	public void testSaveOrUpdate() {
+//		final Set<FilterDto> filterDtos = FilterDtoFactory.createSet(2);
+//		
+//		MessageDto messageDto = MessageDtoFactory.createMessageDto();
+//		
+//		messageDto.setPublishTime(new Date(0));
+//		messageDto.setMessageType(MessageType.RICH_POPUP);
+//		messageDto.setFilterDtos(filterDtos);
+//		
+//		String communityURL ="o2";
+//		Message message = MessageFactory.createMessage(null);
+//		message.setId(Integer.MAX_VALUE);
+//		message.setPosition(Integer.MAX_VALUE);
+//
+//		Community community = CommunityFactory.createCommunity(); 
+//		
+//		when(mockCommunityService.getCommunityByUrl(communityURL)).thenReturn(community);
+//		when(mockMessageRepository.findMaxPosition(community, messageDto.getMessageType(), messageDto.getPublishTime().getTime())).thenReturn(Integer.MIN_VALUE);
+//		when(mockFilterService.find(filterDtos));
+//
+//		final Set<AbstractFilterWithCtiteria> filterWithCtiteria;
+//		if (filterDtos != null)
+//			filterWithCtiteria = filterService.find(filterDtos);
+//		else
+//			filterWithCtiteria = Collections.EMPTY_SET;
+//
+//		message.setTitle(messageDto.getHeadline());
+//		message.setActivated(messageDto.isActivated());
+//		message.setBody(messageDto.getBody());
+//		message.setFrequence(messageDto.getFrequence());
+//		message.setMessageType(messageDto.getMessageType());
+//		message.setPublishTimeMillis(publishTimeMillis);
+//		message.setFilterWithCtiteria(filterWithCtiteria);
+//		message.setPosition(position);
+//		message.setCommunity(community);
+//		message.setAction(messageDto.getAction());
+//		message.setActionType(messageDto.getActionType());
+//		message.setActionButtonText(messageDto.getActionButtonText());
+//
+//		message = messageRepository.save(message);
+//
+//		
+//	}
+	
+	@Test(expected=Exception.class)
+	public void testUpdate_findOneThrowsException_Failure() throws Exception {
+		
+		MessageDto messageDto = MessageDtoFactory.createMessageDto();
+		messageDto.setId(Integer.MAX_VALUE);
+		
+		String communityURL = "o2";
+		
+		Message message = MessageFactory.createMessage(null);
+		
+		when(mockMessageRepository.findOne(messageDto.getId())).thenThrow(new Exception());
+		
+		doReturn(message).when(messageServiceSpy).saveOrUpdate(eq(messageDto), eq(communityURL), any(Message.class));
+		
+		messageServiceSpy.update(messageDto, communityURL);
+	}
+	
 	@Before
 	public void setUp()
 			throws Exception {
@@ -358,13 +521,13 @@ public class MessageServiceTest {
 		mockFilterService = Mockito.mock(FilterService.class);
 		mockCloudFileService = Mockito.mock(CloudFileService.class);
 
-		fixture = new MessageService();
-		fixture.setCloudFileService(mockCloudFileService);
-		fixture.setMessageRepository(mockMessageRepository);
-		fixture.setFilterService(mockFilterService);
-		fixture.setCommunityService(mockCommunityService);
-		fixture.setUserService(new UserService());
-		fixture.dateFormat = new SimpleDateFormat();
+		messageServiceSpy = spy(new MessageService());
+		messageServiceSpy.setCloudFileService(mockCloudFileService);
+		messageServiceSpy.setMessageRepository(mockMessageRepository);
+		messageServiceSpy.setFilterService(mockFilterService);
+		messageServiceSpy.setCommunityService(mockCommunityService);
+		messageServiceSpy.setUserService(new UserService());
+		messageServiceSpy.dateFormat = new SimpleDateFormat();
 		
 		PowerMockito.mockStatic(Utils.class);
 		
