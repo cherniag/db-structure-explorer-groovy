0a9d0688db84927c37d2e3749473ea8f13507863
Mykhailo Titov <mykhayl.titov@musicqubed.com>
2013-06-05 13:33:34 +0300

    [Jadmin] Add rich popup which allows deeplinking to various "anchors" in the app/mobile web
    http://jira.musicqubed.com/browse/IMP-1542
    
    MessageDtoFactory added

---
+++ shared/src/test/java/mobi/nowtechnologies/server/shared/dto/admin/MessageDtoFactory.java
@@ -0,0 +1,13 @@
+package mobi.nowtechnologies.server.shared.dto.admin;
+
+/**
+ * @author Titov Mykhaylo (titov)
+ *
+ */
+public class MessageDtoFactory
+ {
+
+	public static MessageDto createMessageDto() {
+		return new MessageDto();
+	}
+}
