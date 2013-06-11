d66769a0c260df44677d3a6f2971a96dc4b2a0a0
Volodymyr Derkach <vlde@ciklum.com>
2013-06-11 11:55:54 +0300

    IMP-1094: selection play list.

--- web/src/main/webapp/assets/scripts/template-manager.js
+++ web/src/main/webapp/assets/scripts/template-manager.js
@@ -1,16 +1,31 @@
 Templates = {
     templates: {},
+    names:[],
+    mainName: null,
+    loaded: 0,
+    notStarted: true,
 
-    load: function (names) {
-        var that = this;
-        for (var id in names) {
+    pick: function (name, callback) {
+        var T = Templates;
             $.ajax({
-                url: Templates.templatesPath + names[id] + ".htm",
+            url: T.templatesPath + name + ".htm",
                 success: function (data) {
-                    that.templates[names[id]] = data;
+                T.templates[name] = data;
+                T[name] = _.template(data);
+                T.loaded++;
+                if(T.notStarted && (T.loaded == T.names.length || T.mainName == name)){
+                    T.notStarted = false;
+                    callback();
+                }
+            }
+        });
                 },
-                async: false});
-        }
+    load: function (names, mainName, callback) {
+        var T = Templates;
+        T.names = names;
+        T.mainName = mainName;
+        for(var i in T.names)
+            T.pick(T.names[i], callback);
     },
     get: function (name) {
         return this.templates[name];
