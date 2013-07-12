edc20bf8009d412c99f0c4ac953066b6d1766ce0
Mykhailo Titov <mykhayl.titov@musicqubed.com>
2013-07-12 11:45:41 +0300

    version changed

--- persistence/src/main/resources/sql/release4.0-SNAPSHOT.sql
+++ persistence/src/main/resources/sql/release4.0-SNAPSHOT.sql
@@ -1,3 +1,6 @@
  -- Final insert of the release version
 insert into system (release_time_millis, version, release_name) values(unix_timestamp(now()), "4.0-SN", "4.0-SN");
 
+alter table tb_paymentpolicy add column tariff char(255);
+
+update tb_paymentpolicy set tariff='_3G' where provider='o2';
