7c5009a9e8e2a4cba4f47cd078da6f6656eb9fa0
Volodymyr Derkach <vlde@ciklum.com>
2013-07-19 17:06:34 +0300

    IMP-1785: [Server] Add new promotion types for 4G users

--- persistence/src/main/resources/sql/release4.0-SNAPSHOT.sql
+++ persistence/src/main/resources/sql/release4.0-SNAPSHOT.sql
@@ -63,12 +63,12 @@ create table data_to_do_refund (
  alter table data_to_do_refund add index data_to_do_refund_PK_user_id (user_id), add constraint data_to_do_refund_U_user_id foreign key (user_id) references tb_users (i);
 
  -- IMP-1785: [Server] Add new promotion types for 4G users
- insert into tb_promotions(description, startDate, endDate, isActive, freeWeeks, userGroup, type, label)
-   value('o2 Free Trial for direct users', unix_timestamp(), 1606780800, true, 52, 10, 'PromoCode', 'o2_direct');
- insert into tb_promotions(i, description, startDate, endDate, isActive, freeWeeks, userGroup, type, label)
-   value('o2 Free Trial for indirect users', unix_timestamp(), 1606780800, true, 8, 10, 'PromoCode', 'o2_indirect');
- insert into tb_promoCode(code, promotionId) select label, i from tb_promoCode where label = 'o2_direct';
- insert into tb_promoCode(code, promotionId) select label, i from tb_promoCode where label = 'o2_indirect';
+ insert into tb_promotions(description, startDate, endDate, isActive, freeWeeks, userGroup, type, label, numUsers, maxUsers, subWeeks, showPromotion)
+   value('o2 Free Trial for direct users', unix_timestamp(), 1606780800, true, 52, 10, 'PromoCode', 'o2_direct', 0, 0, 0, false);
+ insert into tb_promotions(i, description, startDate, endDate, isActive, freeWeeks, userGroup, type, label,  numUsers, maxUsers, subWeeks, showPromotion)
+   value('o2 Free Trial for indirect users', unix_timestamp(), 1606780800, true, 8, 10, 'PromoCode', 'o2_indirect', 0, 0, 0, false);
+ insert into tb_promoCode(code, promotionId) select label, i from tb_promotions where label = 'o2_direct';
+ insert into tb_promoCode(code, promotionId) select label, i from tb_promotions where label = 'o2_indirect';
 
  --
  alter table tb_users add column contractChannel varchar(255) default 'DIRECT';
