b106693bb4a8e3e2751382435c789fb3d0d0644d
Alexander Kolpakov <kalex@ciklum.com>
2013-07-22 09:07:25 +0300

    add cn_service_admin.sql script

---
+++ admin/src/main/resources/conf/admin-release.sql
@@ -0,0 +1,9 @@
+INSERT INTO cn_service_admin.authorities (username, authority) VALUES ('admin', 'ROLE_ADMIN');
+
+INSERT INTO cn_service_admin.users (username, communityURL, password, enabled) VALUES ('admin', 'chartsnow', 'ceb4f32325eda6142bd65215f4c0f371', true);
+INSERT INTO cn_service_admin.users (username, communityURL, password, enabled) VALUES ('admin', 'metalhammer', 'ceb4f32325eda6142bd65215f4c0f371', true);
+INSERT INTO cn_service_admin.users (username, communityURL, password, enabled) VALUES ('admin', 'mobo', 'ceb4f32325eda6142bd65215f4c0f371', true);
+INSERT INTO cn_service_admin.users (username, communityURL, password, enabled) VALUES ('admin', 'o2', 'ceb4f32325eda6142bd65215f4c0f371', true);
+INSERT INTO cn_service_admin.users (username, communityURL, password, enabled) VALUES ('admin', 'occ', 'ceb4f32325eda6142bd65215f4c0f371', true);
+INSERT INTO cn_service_admin.users (username, communityURL, password, enabled) VALUES ('admin', 'runningtrax', 'ceb4f32325eda6142bd65215f4c0f371', true);
+INSERT INTO cn_service_admin.users (username, communityURL, password, enabled) VALUES ('admin', 'samsung', 'ceb4f32325eda6142bd65215f4c0f371', true);
