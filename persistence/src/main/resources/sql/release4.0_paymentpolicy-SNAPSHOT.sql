00930debb04517f9c1417c0efce8b3510cbf8bfe
Adrian Zavelcuta <adrian.zavelcuta@musicqubed.com>
2013-07-22 14:40:46 +0100

    sql changes for 4G payment policies (video)

---
+++ persistence/src/main/resources/sql/release4.0_paymentpolicy-SNAPSHOT.sql
@@ -0,0 +1,12 @@
+-- insert 3 new video payment policies for o2 consumer group
+
+INSERT INTO PressCtrlH (communityID,subWeeks,subCost,paymentType,operator,shortCode,currencyIso,availableInStore,app_store_product_id,contract,segment,content_category,content_type,content_description,sub_merchant_id,provider,tariff) VALUES
+(10,5,'7.5','o2Psms',null,'','GBP',true,null,null,'CONSUMER','other','mqbed_tracks_3107056','Description of content','O2 Tracks','o2','_4G');
+
+
+INSERT INTO PressCtrlH (communityID,subWeeks,subCost,paymentType,operator,shortCode,currencyIso,availableInStore,app_store_product_id,contract,segment,content_category,content_type,content_description,sub_merchant_id,provider,tariff) VALUES
+(10,2,'3','o2Psms',null,'','GBP',true,null,null,'CONSUMER','other','mqbed_tracks_3107055','Description of content','O2 Tracks','o2','_4G');
+
+
+INSERT INTO PressCtrlH (communityID,subWeeks,subCost,paymentType,operator,shortCode,currencyIso,availableInStore,app_store_product_id,contract,segment,content_category,content_type,content_description,sub_merchant_id,provider,tariff) VALUES
+(10,1,'1.5','o2Psms',null,'','GBP',true,null,null,'CONSUMER','other','mqbed_tracks_3107054','Description of content','O2 Tracks','o2','_4G');
