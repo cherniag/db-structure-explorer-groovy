320f198d1922566dfe9ed2c3371616037b46db13
Volodymyr Derkach <vlde@ciklum.com>
2013-07-22 17:53:08 +0300

    IMP-1779: [Jadmin] Allow content manager user to create chart updates with Video

--- persistence/src/main/java/mobi/nowtechnologies/server/persistence/repository/MediaRepository.java
+++ persistence/src/main/java/mobi/nowtechnologies/server/persistence/repository/MediaRepository.java
@@ -17,6 +17,9 @@ public interface MediaRepository extends JpaRepository<Media, Integer> {
 	@Query(value = "select media from Media media join FETCH media.artist artist join FETCH media.imageFileSmall imageFileSmall where media.title like :searchWords or media.isrc like :searchWords or artist.name like :searchWords")
 	List<Media> getMedias(@Param("searchWords") String searchWords);
 
+    @Query(value = "select media from Media media join FETCH media.artist artist join FETCH media.imageFileSmall imageFileSmall where media.audioFile.fileType.i = :type and media.title like :searchWords or media.isrc like :searchWords or artist.name like :searchWords")
+    List<Media> getMedias(@Param("searchWords") String searchWords, @Param("type")Byte type);
+
 	@Query(value = "select media from Media media where media.isrc = :isrc")
 	Media getByIsrc(@Param("isrc")String isrc);
 
