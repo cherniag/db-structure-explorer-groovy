cd7f32680803ea04462005b3ca052c0a10adc230
Adrian Zavelcuta <adrian.zavelcuta@musicqubed.com>
2013-07-25 09:32:29 +0100

    sql changes moved to release4.0-SNAPSHOT.sql

--- persistence/src/main/resources/sql/release4.0-SNAPSHOT.sql
+++ persistence/src/main/resources/sql/release4.0-SNAPSHOT.sql
@@ -83,3 +83,39 @@ create table refund (
 
  -- IMP-1781 [Track Repo] Migrate tracks ingestion from CMS to Track Repo
  insert into tb_fileTypes (i, name) value (4, 'VIDEO');
+ 
+ 
+ ALTER TABLE tb_paymentPolicy ADD COLUMN mediatype VARCHAR(25) NULL DEFAULT 'AUDIO';
+
+-- insert 3 new audio payment policies for o2 consumer group
+
+INSERT INTO tb_paymentPolicy (communityID,subWeeks,subCost,paymentType,operator,shortCode,currencyIso,availableInStore,app_store_product_id,contract,segment,content_category,content_type,content_description,sub_merchant_id,provider,tariff,mediatype) VALUES
+(10,5,5,'o2Psms',null,'','GBP',true,null,null,'CONSUMER','other','mqbed_tracks_3107056','Description of content','O2 Tracks','o2','_4G','AUDIO');
+
+
+INSERT INTO tb_paymentPolicy (communityID,subWeeks,subCost,paymentType,operator,shortCode,currencyIso,availableInStore,app_store_product_id,contract,segment,content_category,content_type,content_description,sub_merchant_id,provider,tariff,mediatype) VALUES
+(10,2,2,'o2Psms',null,'','GBP',true,null,null,'CONSUMER','other','mqbed_tracks_3107055','Description of content','O2 Tracks','o2','_4G','AUDIO');
+
+
+INSERT INTO tb_paymentPolicy (communityID,subWeeks,subCost,paymentType,operator,shortCode,currencyIso,availableInStore,app_store_product_id,contract,segment,content_category,content_type,content_description,sub_merchant_id,provider,tariff,mediatype) VALUES
+(10,1,1,'o2Psms',null,'','GBP',true,null,null,'CONSUMER','other','mqbed_tracks_3107054','Description of content','O2 Tracks','o2','_4G','AUDIO');
+
+
+
+-- insert 3 new audio+video payment policies for o2 consumer group
+
+INSERT INTO tb_paymentPolicy (communityID,subWeeks,subCost,paymentType,operator,shortCode,currencyIso,availableInStore,app_store_product_id,contract,segment,content_category,content_type,content_description,sub_merchant_id,provider,tariff,mediatype) VALUES
+(10,3,'4.5','o2Psms',null,'','GBP',true,null,null,'CONSUMER','other','mqbed_tracks_3107059','Description of content','O2 Tracks','o2','_4G','AUDIOPLUSVIDEO');
+
+
+INSERT INTO tb_paymentPolicy (communityID,subWeeks,subCost,paymentType,operator,shortCode,currencyIso,availableInStore,app_store_product_id,contract,segment,content_category,content_type,content_description,sub_merchant_id,provider,tariff,mediatype) VALUES
+(10,2,'3','o2Psms',null,'','GBP',true,null,null,'CONSUMER','other','mqbed_tracks_3107058','Description of content','O2 Tracks','o2','_4G','AUDIOPLUSVIDEO');
+
+
+INSERT INTO tb_paymentPolicy (communityID,subWeeks,subCost,paymentType,operator,shortCode,currencyIso,availableInStore,app_store_product_id,contract,segment,content_category,content_type,content_description,sub_merchant_id,provider,tariff,mediatype) VALUES
+(10,1,'1.5','o2Psms',null,'','GBP',true,null,null,'CONSUMER','other','mqbed_tracks_3107057','Description of content','O2 Tracks','o2','_4G','AUDIOPLUSVIDEO');
+
+
+
+-- add a new column to Users to see if the user subscribes/unsubscribes to 4G
+ALTER TABLE tb_users ADD COLUMN optedInForVideo TINYINT(1) NULL DEFAULT NULL;
