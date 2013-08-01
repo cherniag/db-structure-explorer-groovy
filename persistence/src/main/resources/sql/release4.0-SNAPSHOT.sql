2f21d2a5a3b5dfdf20f85fa5f385ed2879dc9ecd
Alexander Kolpakov <kalex@ciklum.com>
2013-08-01 11:32:08 +0300

    add device type to migrate script

--- persistence/src/main/resources/sql/release4.0-SNAPSHOT.sql
+++ persistence/src/main/resources/sql/release4.0-SNAPSHOT.sql
@@ -122,4 +122,5 @@ INSERT INTO tb_paymentPolicy (communityID,subWeeks,subCost,paymentType,operator,
 INSERT INTO tb_paymentPolicy (communityID,subWeeks,subCost,paymentType,operator,shortCode,currencyIso,availableInStore,app_store_product_id,contract,segment,content_category,content_type,content_description,sub_merchant_id,provider,tariff,media_type) VALUES
 (10,1,'1.5','o2Psms',null,'','GBP',true,null,null,'CONSUMER','other','mqbed_tracks_3107057','Description of content','O2 Tracks','o2','_4G','VIDEO_AND_AUDIO');
 
-
+ -- insert new windows phone device type to database
+INSERT INTO tb_deviceTypes (i, name) VALUES (7,'WINDOWS_PHONE');
