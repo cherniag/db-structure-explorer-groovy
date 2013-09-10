fdea0427dbcb92883b57424a8a10c416cc5edf4e
Mykhailo Titov <mykhayl.titov@musicqubed.com>
2013-09-10 16:46:27 +0300

    IMP-1539 - New label set up - Absolute

--- track-repo/src/test/java/mobi/nowtechnologies/server/trackrepo/ingest/absolute/AbsoluteParserTest.java
+++ track-repo/src/test/java/mobi/nowtechnologies/server/trackrepo/ingest/absolute/AbsoluteParserTest.java
@@ -10,8 +10,6 @@ import org.custommonkey.xmlunit.exceptions.XpathException;
 import org.junit.Ignore;
 import org.junit.Test;
 import org.springframework.core.io.ClassPathResource;
-import org.w3c.dom.Element;
-import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
 import java.io.FileNotFoundException;
@@ -19,7 +17,8 @@ import java.util.List;
 import java.util.Map;
 
 import static java.lang.Integer.parseInt;
-import static mobi.nowtechnologies.server.trackrepo.ingest.DropTrack.*;
+import static mobi.nowtechnologies.server.trackrepo.domain.AssetFile.*;
+import static mobi.nowtechnologies.server.trackrepo.domain.AssetFile.FileType.*;
 import static mobi.nowtechnologies.server.trackrepo.ingest.DropTrack.Type.*;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertNotNull;
@@ -29,6 +28,12 @@ public class AbsoluteParserTest extends ParserTest<AbsoluteParser> {
 
     private Map<String,DropTrack> resultDropTrackMap;
     private NodeList expectedTrackReleaseIdNodeList;
+    private DropTrack resultDropTrack;
+    private int xPathExpressionResultDropTrackIndex;
+    private String expectedIsrc;
+    private String expectedDistributor;
+    private String expectedLabel;
+    private int expectedResultDropTrackIndex;
 
     public void createParser() throws FileNotFoundException {
         parserFixture = new AbsoluteParser("classpath:media/absolute/");
@@ -39,7 +44,6 @@ public class AbsoluteParserTest extends ParserTest<AbsoluteParser> {
     }
 
     @Test
-    @Ignore
     public void verifyThatAbsoluteParserReadBasicFieldCorrectly() throws Exception {
         //given
         xmlFile = new ClassPathResource("media/absolute/absolute.xml").getFile();
@@ -58,57 +62,32 @@ public class AbsoluteParserTest extends ParserTest<AbsoluteParser> {
         assertThat(resultDropTrackMap.size(), is(getTrackReleaseCount()));
 
         expectedTrackReleaseIdNodeList = getTrackReleaseIdNodeList();
+        expectedDistributor = getDistributor();
 
-        for (int i = 0; i < expectedTrackReleaseIdNodeList.getLength(); i++) {
-            validateResultDropTrack(i);
+        for (expectedResultDropTrackIndex = 0; expectedResultDropTrackIndex < expectedTrackReleaseIdNodeList.getLength(); expectedResultDropTrackIndex++) {
+            validateResultDropTrack();
         }
-
-        DropTrack dropTrack = resultDropTrackMap.get("ROROT1302001_AbsoluteParser");
-
-        List<DropTerritory> territories = dropTrack.getTerritories();
-
-        assertThat(territories.size(), is(2));
-        DropTerritory territory = territories.get(0);
-        assertThat(territory.country, is("GB"));   // IE
-        assertThat(territory.currency, is("GBP")); // GBP default
-//        assertThat(territory.dealReference, is("")); //DealTerms
-        assertThat(territory.distributor, is("Absolute Marketing & Distribution Ltd."));
-        assertThat(territory.label, is("3 Beat Productions"));
-        assertThat(territory.price, is(0.0f));    //default
-        assertThat(territory.priceCode, is("0.0"));
-        assertThat(territory.publisher, is(""));
-        assertThat(territory.reportingId, is("ROROT1302001"));
-        assertThat(territory.startdate, is(DateUtils.newDate(28, 07, 2013)));  // <StartDate>2013-07-28</StartDate>
-        assertThat(territory.takeDown, is(false));
-
-        List<DropAssetFile> files = dropTrack.getFiles();
-        DropAssetFile asset = files.get(0);
-        assertThat(asset.duration, is(0)); //PT3M50S
-        assertThat(asset.file, is(""));
-        assertThat(asset.isrc, is(""));
-        assertThat(asset.md5, is(""));
-        assertThat(asset.type, is(AssetFile.FileType.MOBILE));
     }
 
-    private void validateResultDropTrack(int i) throws XpathException {
-        int xPathExpressionIndex = i + 1;
-        String expectedIsrc = getIsrc(xPathExpressionIndex);
+    private void validateResultDropTrack() throws XpathException {
+        xPathExpressionResultDropTrackIndex = expectedResultDropTrackIndex + 1;
+        expectedIsrc = getIsrc(xPathExpressionResultDropTrackIndex);
+        expectedLabel = getLabel(xPathExpressionResultDropTrackIndex);
 
-        DropTrack resultDropTrack = getResultDropTrack(expectedIsrc);
+        resultDropTrack = getResultDropTrack(expectedIsrc);
 
         assertNotNull(resultDropTrack);
-
         //assertThat(resultDropTrack.xml, is("3BEATCD019"));
         assertThat(resultDropTrack.type, is(INSERT));
         assertThat(resultDropTrack.productCode, is(""));
-        assertThat(resultDropTrack.title, is(getTitleText(xPathExpressionIndex)));
-        assertThat(resultDropTrack.subTitle, is(getSubTitle(xPathExpressionIndex)));
-        assertThat(resultDropTrack.artist, is(getArtist(xPathExpressionIndex)));
-        assertThat(resultDropTrack.genre, is(getGenre(xPathExpressionIndex)));
-        assertThat(resultDropTrack.copyright, is(getCopyright(xPathExpressionIndex)));
-        assertThat(resultDropTrack.label, is(getLabel(xPathExpressionIndex)));
+        assertThat(resultDropTrack.title, is(getTitleText(xPathExpressionResultDropTrackIndex)));
+        assertThat(resultDropTrack.subTitle, is(getSubTitle(xPathExpressionResultDropTrackIndex)));
+        assertThat(resultDropTrack.artist, is(getArtist(xPathExpressionResultDropTrackIndex)));
+        assertThat(resultDropTrack.genre, is(getGenre(xPathExpressionResultDropTrackIndex)));
+        assertThat(resultDropTrack.copyright, is(getCopyright(xPathExpressionResultDropTrackIndex)));
+        assertThat(resultDropTrack.label, is(expectedLabel));
         assertThat(resultDropTrack.isrc, is(expectedIsrc));
-        assertThat(resultDropTrack.year, is(getYear(xPathExpressionIndex)));
+        assertThat(resultDropTrack.year, is(getYear(xPathExpressionResultDropTrackIndex)));
         assertThat(resultDropTrack.physicalProductId, is(expectedIsrc));
         // assertThat(resultDropTrack.album, is("Party Never Ends"));
         assertThat(resultDropTrack.info, is(""));
@@ -117,6 +96,43 @@ public class AbsoluteParserTest extends ParserTest<AbsoluteParser> {
         assertThat(resultDropTrack.explicit, is(false));
         assertThat(resultDropTrack.productId, is(expectedIsrc));
 
+        validateResultTerritories();
+        //validateResultFiles();
+    }
+
+    private void validateResultFiles() {
+        List<DropAssetFile> files = resultDropTrack.getFiles();
+
+        DropAssetFile asset = files.get(0);
+        assertThat(asset.duration, is(0)); //PT3M50S
+        assertThat(asset.file, is(""));
+        assertThat(asset.isrc, is(""));
+        assertThat(asset.md5, is(""));
+        assertThat(asset.type, is(MOBILE));
+    }
+
+    private void validateResultTerritories() throws XpathException {
+        List<DropTerritory> territories = resultDropTrack.getTerritories();
+
+        assertNotNull(territories);
+        assertThat(territories.size(), is(getTerritoryPerTrackCount(xPathExpressionResultDropTrackIndex)));
+
+        for (int i = 0; i < territories.size(); i++) {
+            DropTerritory territory = territories.get(i);
+
+            int territoryPerTrackIndex = i + 1;
+            assertThat(territory.country, is(getTerritoryPerTrack(xPathExpressionResultDropTrackIndex, territoryPerTrackIndex)));
+            assertThat(territory.currency, is("GBP"));
+//        assertThat(territory.dealReference, is("")); //DealTerms
+            assertThat(territory.distributor, is(expectedDistributor));
+            assertThat(territory.label, is(expectedLabel));
+            assertThat(territory.price, is(0.0f));
+            assertThat(territory.priceCode, is("0.0"));
+            assertThat(territory.publisher, is(""));
+            assertThat(territory.reportingId, is(expectedIsrc));
+//            assertThat(territory.startdate, is(DateUtils.newDate(28, 07, 2013)));
+            assertThat(territory.takeDown, is(false));
+        }
     }
 
     private String getTitleText(int index) throws XpathException {
@@ -149,6 +165,18 @@ public class AbsoluteParserTest extends ParserTest<AbsoluteParser> {
 
     private String getYear(int index) throws XpathException {
         return evaluate("(/ern:NewReleaseMessage/ResourceList/SoundRecording/SoundRecordingDetailsByTerritory/PLine/Year)[" + index +"]");
+    }
+
+    private int getTerritoryPerTrackCount(int index) throws XpathException {
+        return parseInt(evaluate("count((/ern:NewReleaseMessage/ResourceList/SoundRecording/SoundRecordingDetailsByTerritory)[" + index +"]/TerritoryCode)"));
+    }
+
+    private String getTerritoryPerTrack(int soundRecordingDetailsByTerritoryIndex, int index) throws XpathException {
+        return evaluate("((/ern:NewReleaseMessage/ResourceList/SoundRecording/SoundRecordingDetailsByTerritory)[" + soundRecordingDetailsByTerritoryIndex +"]/TerritoryCode)["+index +"]");
+    }
+
+    private String getDistributor() throws XpathException {
+        return evaluate("/ern:NewReleaseMessage/MessageHeader/MessageSender/PartyName/FullName");
     }
 
     private int getTrackReleaseCount() throws XpathException {
