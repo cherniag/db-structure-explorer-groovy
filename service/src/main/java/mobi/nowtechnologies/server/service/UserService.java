ff219c93a088be9cbab76aa6866a490ddda651e0
Alexander Kolpakov <kalex@ciklum.com>
2013-09-25 14:39:28 +0300

    unmerge idfa

--- service/src/main/java/mobi/nowtechnologies/server/service/UserService.java
+++ service/src/main/java/mobi/nowtechnologies/server/service/UserService.java
@@ -1795,17 +1795,6 @@ public class UserService {
 		userRepository.updateLastBefore48SmsMillis(lastBefore48SmsMillis, userId);
 	}
 
-    @Transactional
-    public User updateTockenDetails(User user, String idfa) {
-        int result = userRepository.updateTockenDetails(user.getId(), idfa);
-
-        if(result > 0){
-            user.setIdfa(idfa);
-        }
-
-        return user;
-    }
-
 	@Transactional(readOnly = true)
 	public List<User> getUsersForRetryPayment() {
 
