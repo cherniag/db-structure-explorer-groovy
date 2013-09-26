296a46e77a5e08c0e510dac4d6e39298fea0c137
Mykhailo Titov <mykhayl.titov@musicqubed.com>
2013-09-26 18:19:20 +0300

    IMP-2474 - Merge Master branch to the IMP1783 branch

--- persistence/src/main/resources/sql/release4.2-SNAPSHOT.sql
+++ persistence/src/main/resources/sql/release4.2-SNAPSHOT.sql
@@ -1,9 +1,5 @@
  -- Final insert of the release version
-insert into system (release_time_millis, version, release_name) values(unix_timestamp(now()), "4.1-SN", "4.1-SN");
-
--- Allow whitestisted MSISDNs have access to video and locked tracks
--- http://jira.musicqubed.com/browse/IMP-2311
-alter table tb_promotions add column is_white_listed BIT default false;
+insert into system (release_time_millis, version, release_name) values(unix_timestamp(now()), "4.2-SN", "4.2-SN");
 
 -- http://jira.musicqubed.com/browse/IMP-1783
 -- IMP-1783 - [Server] Auto-subscribe new O2 4G and non-4G users activating free trial
