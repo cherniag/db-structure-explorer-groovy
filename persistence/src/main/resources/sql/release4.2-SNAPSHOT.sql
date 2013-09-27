b51e848b11cf9325f78b7ba05eb15034a07d5aa8
Mykhailo Titov <mykhayl.titov@musicqubed.com>
2013-09-27 11:27:44 +0300

    bug  with release4.2-SNAPSHOT.sql fixed

--- persistence/src/main/resources/sql/release4.2-SNAPSHOT.sql
+++ persistence/src/main/resources/sql/release4.2-SNAPSHOT.sql
@@ -5,15 +5,15 @@ insert into system (release_time_millis, version, release_name) values(unix_time
 -- IMP-1783 - [Server] Auto-subscribe new O2 4G and non-4G users activating free trial
 alter table tb_paymentPolicy add column is_default bit default false;
 
-ALTER TABLE tb_paymentDetails DROP FOREIGN KEY FK60BC1B4D10EFFD28;
-ALTER TABLE tb_promotionpaymentpolicy_tb_paymentpolicy DROP FOREIGN KEY FK834CEA00C875CF15;
+ALTER TABLE cn_service.tb_paymentDetails DROP INDEX FK60BC1B4D10EFFD28;
+ALTER TABLE cn_service.tb_promotionPaymentPolicy_tb_paymentPolicy DROP INDEX FK834CEA00C875CF15;
 
 alter table tb_paymentPolicy modify column i int not null auto_increment;
 alter table tb_paymentDetails modify column paymentPolicyId int;
-alter table tb_promotionpaymentpolicy_tb_paymentpolicy modify column paymentPolicies_i int default null;
+alter table tb_promotionPaymentPolicy_tb_paymentPolicy modify column paymentPolicies_i int default null;
 
-alter table tb_paymentDetails add index tb_paymentDetails_PK_paymentPolicyId (paymentPolicyId), add constraint tb_paymentDetails_U_paymentPolicyId foreign key (paymentPolicyId) references tb_paymentPolicy (i);
-alter table tb_promotionpaymentpolicy_tb_paymentpolicy add index tb_promotionpaymentpolicy_tb_paymentpolicy_PK_paymentPolicies_i (paymentPolicies_i), add constraint tb_promotionpaymentpolicy_tb_paymentpolicy_U_paymentPolicies_i foreign key (paymentPolicies_i) references tb_paymentPolicy (i);
+alter table tb_paymentDetails add index tb_paymentDetails_PK_paymentPolicyId (paymentPolicyId);
+alter table tb_promotionPaymentPolicy_tb_paymentPolicy add index tb_promotionPaymentPolicy_tb_paymentPolicy_PK_paymentPolicies_i (paymentPolicies_i);
 
 start transaction;
 
