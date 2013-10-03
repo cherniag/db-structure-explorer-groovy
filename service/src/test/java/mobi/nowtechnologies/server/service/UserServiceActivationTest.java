1765bbb0b9a81842eca10884a703efd61d52fcb4
Alexander Kolpakov <kalex@ciklum.com>
2013-10-03 18:25:49 +0300

    fix tests

--- service/src/test/java/mobi/nowtechnologies/server/service/UserServiceActivationTest.java
+++ service/src/test/java/mobi/nowtechnologies/server/service/UserServiceActivationTest.java
@@ -1,16 +1,6 @@
 package mobi.nowtechnologies.server.service;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-import static org.mockito.Matchers.any;
-import static org.mockito.Matchers.anyString;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
-import mobi.nowtechnologies.server.persistence.dao.DeviceTypeDao;
-import mobi.nowtechnologies.server.persistence.dao.OperatorDao;
-import mobi.nowtechnologies.server.persistence.dao.UserDao;
-import mobi.nowtechnologies.server.persistence.dao.UserGroupDao;
-import mobi.nowtechnologies.server.persistence.dao.UserStatusDao;
+import mobi.nowtechnologies.server.persistence.dao.*;
 import mobi.nowtechnologies.server.persistence.domain.AccountLog;
 import mobi.nowtechnologies.server.persistence.domain.User;
 import mobi.nowtechnologies.server.persistence.domain.UserFactory;
@@ -18,9 +8,11 @@ import mobi.nowtechnologies.server.persistence.domain.enums.ProviderType;
 import mobi.nowtechnologies.server.persistence.domain.enums.SegmentType;
 import mobi.nowtechnologies.server.persistence.repository.UserBannedRepository;
 import mobi.nowtechnologies.server.persistence.repository.UserRepository;
+import mobi.nowtechnologies.server.service.data.PhoneNumberValidationData;
 import mobi.nowtechnologies.server.service.o2.O2Service;
 import mobi.nowtechnologies.server.service.o2.impl.O2ProviderService;
 import mobi.nowtechnologies.server.service.o2.impl.O2SubscriberData;
+import mobi.nowtechnologies.server.service.o2.impl.O2UserDetailsUpdater;
 import mobi.nowtechnologies.server.service.payment.MigPaymentService;
 import mobi.nowtechnologies.server.service.payment.http.MigHttpService;
 import mobi.nowtechnologies.server.shared.Utils;
@@ -28,7 +20,6 @@ import mobi.nowtechnologies.server.shared.enums.ActivationStatus;
 import mobi.nowtechnologies.server.shared.enums.Tariff;
 import mobi.nowtechnologies.server.shared.message.CommunityResourceBundleMessageSource;
 import mobi.nowtechnologies.server.shared.util.EmailValidator;
-
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -38,6 +29,13 @@ import org.mockito.stubbing.Answer;
 import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
 @SuppressWarnings("deprecation")
 @RunWith(PowerMockRunner.class)
@@ -67,8 +65,9 @@ public class UserServiceActivationTest {
 	private RefundService refundServiceMock;
 
 	private PromotionService promotionServiceMock;
+    private O2UserDetailsUpdater o2UserDetailsUpdaterMock;
 
-	@Before
+    @Before
 	public void setUp() throws Exception {
 		userServiceSpy = Mockito.spy(new UserService());
 
@@ -100,6 +99,7 @@ public class UserServiceActivationTest {
 		iTunesServiceMock = PowerMockito.mock(ITunesService.class);
 		userBannedRepositoryMock = PowerMockito.mock(UserBannedRepository.class);
 		refundServiceMock = PowerMockito.mock(RefundService.class);
+        o2UserDetailsUpdaterMock = PowerMockito.mock(O2UserDetailsUpdater.class);
 
 		userServiceSpy.setPaymentPolicyService(paymentPolicyServiceMock);
 		userServiceSpy.setCountryService(countryServiceMock);
@@ -129,6 +129,8 @@ public class UserServiceActivationTest {
 		userServiceSpy.setiTunesService(iTunesServiceMock);
 		userServiceSpy.setUserBannedRepository(userBannedRepositoryMock);
 		userServiceSpy.setRefundService(refundServiceMock);
+        userServiceSpy.setMobileProviderService(o2ClientServiceMock);
+        userServiceSpy.setUserDetailsUpdater(o2UserDetailsUpdaterMock);
 
 		PowerMockito.mockStatic(UserStatusDao.class);
 	}
@@ -140,12 +142,12 @@ public class UserServiceActivationTest {
 		user.setMobile(phoneNumber);
 		user.setUserName(phoneNumber);
 
-		Mockito.when(o2ClientServiceMock.validatePhoneNumber(anyString())).thenAnswer(new Answer<String>() {
+		Mockito.when(o2ClientServiceMock.validatePhoneNumber(anyString())).thenAnswer(new Answer<PhoneNumberValidationData>() {
 			@Override
-			public String answer(InvocationOnMock invocation) throws Throwable {
+			public PhoneNumberValidationData answer(InvocationOnMock invocation) throws Throwable {
 				String phone = (String) invocation.getArguments()[0];
 				assertEquals(user.getMobile(), phone);
-				return phoneNumber;
+				return new PhoneNumberValidationData().withPhoneNumber(phoneNumber);
 			}
 		});
 
@@ -154,7 +156,7 @@ public class UserServiceActivationTest {
 		o2SubscriberData.setContractPostPayOrPrePay(true);
 		o2SubscriberData.setTariff4G(false);
 
-		Mockito.when(o2ServiceMock.getSubscriberData(anyString())).thenAnswer(new Answer<O2SubscriberData>() {
+		Mockito.when(o2ClientServiceMock.getSubscriberData(anyString())).thenAnswer(new Answer<O2SubscriberData>() {
 			@Override
 			public O2SubscriberData answer(InvocationOnMock invocation) throws Throwable {
 
@@ -174,7 +176,7 @@ public class UserServiceActivationTest {
 
 		verify(userRepositoryMock, times(1)).save(any(User.class));
 		verify(o2ClientServiceMock, times(1)).validatePhoneNumber(anyString());
-		verify(o2ServiceMock, times(1)).getSubscriberData(anyString());
+		verify(o2ClientServiceMock, times(1)).getSubscriberData(anyString());
 		assertEquals(user.getSegment(), SegmentType.CONSUMER);
 		assertEquals(user.getProvider(), ProviderType.O2.toString());
 		assertEquals(user.getTariff(), Tariff._3G);
