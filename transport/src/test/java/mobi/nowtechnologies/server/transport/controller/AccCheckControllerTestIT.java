d6999f1ff5031c00604dec2496d93f9e224c5201
Alexander Kolpakov <kalex@ciklum.com>
2013-10-07 15:38:02 +0300

    fix hasAllDetails flag

--- transport/src/test/java/mobi/nowtechnologies/server/transport/controller/AccCheckControllerTestIT.java
+++ transport/src/test/java/mobi/nowtechnologies/server/transport/controller/AccCheckControllerTestIT.java
@@ -198,4 +198,33 @@ public class AccCheckControllerTestIT {
 
         assertTrue(resultXml.contains("<deviceUID>fail</deviceUID>"));
     }
+
+    @Test
+    public void testAccountCheckForFVClient_HasAllDetails_Success() throws Exception {
+        String userName = "+642111111111";
+        String phone = "+642111111111";
+        String apiVersion = "5.0";
+        String communityName = "vf_nz";
+        String communityUrl = "vf_nz";
+        String timestamp = "2011_12_26_07_04_23";
+        String storedToken = "f701af8d07e5c95d3f5cf3bd9a62344d";
+        String userToken = Utils.createTimestampToken(storedToken, timestamp);
+
+        User user = userService.findByNameAndCommunity(userName, communityName);
+        user.setProvider("non_vf");
+        userService.updateUser(user);
+
+        ResultActions resultActions = mockMvc.perform(
+                post("/"+communityUrl+"/"+apiVersion+"/PHONE_NUMBER")
+                        .param("USER_NAME", userName)
+                        .param("USER_TOKEN", userToken)
+                        .param("TIMESTAMP", timestamp)
+                        .param("PHONE", phone)
+        ).andExpect(status().isOk());
+
+        MockHttpServletResponse aHttpServletResponse = resultActions.andReturn().getResponse();
+        String resultXml = aHttpServletResponse.getContentAsString();
+
+        assertTrue(resultXml.contains("<hasAllDetails>true</hasAllDetails>"));
+    }
 }
