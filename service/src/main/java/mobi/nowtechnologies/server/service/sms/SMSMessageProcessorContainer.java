3aa28cb89dab92e393ac454855ccf3b24299ecf8
Alexander Kolpakov <kalex@ciklum.com>
2013-11-04 14:26:28 +0200

    IMP-2741 [Backend] Provide a configurable connection pool for sending and receiving VF messages

--- service/src/main/java/mobi/nowtechnologies/server/service/sms/SMSMessageProcessorContainer.java
+++ service/src/main/java/mobi/nowtechnologies/server/service/sms/SMSMessageProcessorContainer.java
@@ -1,11 +1,14 @@
 package mobi.nowtechnologies.server.service.sms;
 
+import com.sentaca.spring.smpp.jsmpp.JSMPPGateway;
 import com.sentaca.spring.smpp.mo.DeliverSmMessageProcessor;
 import com.sentaca.spring.smpp.mo.MOMessage;
 import org.jsmpp.bean.DeliverSm;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.smslib.smpp.AbstractSMPPGateway;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class SMSMessageProcessorContainer extends DeliverSmMessageProcessor {
@@ -13,11 +16,23 @@ public class SMSMessageProcessorContainer extends DeliverSmMessageProcessor {
 
     protected List<SMSMessageProcessor> processors;
 
+    protected List<AbstractSMPPGateway> gateways;
+
+    private boolean alreadyCalled;
+
     @Override
     public void processInboundMessage(DeliverSm deliverSm, MOMessage inboundMessage) {
         LOGGER.info("SMSMessageProcessorContainer has got sms message: [{}]", new Object[]{inboundMessage});
 
         processMessage(deliverSm, inboundMessage);
+
+        int i = 0;
+        for(AbstractSMPPGateway gateway : gateways){
+            if(i > 0)
+                gateway.incInboundMessageCount();
+
+            i++;
+        }
     }
 
     protected void processMessage(final DeliverSm deliverSm, Object message){
@@ -36,5 +51,14 @@ public class SMSMessageProcessorContainer extends DeliverSmMessageProcessor {
 
     public void setMessageProcessors(List<SMSMessageProcessor> processors) {
         this.processors = processors;
+    }
+
+    public void setJsmppGateway(AbstractSMPPGateway jsmppGateway) {
+        if (!alreadyCalled) {
+            gateways = new ArrayList<AbstractSMPPGateway>();
+            super.setJsmppGateway((JSMPPGateway)jsmppGateway);
+        }
+        gateways.add(jsmppGateway);
+        alreadyCalled = true;
     }
 }
--- transport/src/test/java/mobi/nowtechnologies/server/transport/controller/PhoneNumberControllerTestIT.java
+++ transport/src/test/java/mobi/nowtechnologies/server/transport/controller/PhoneNumberControllerTestIT.java
@@ -21,7 +21,6 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.transaction.TransactionConfiguration;
 import org.springframework.test.web.server.MockMvc;
 import org.springframework.test.web.server.ResultActions;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.context.WebApplicationContext;
 
 import static org.junit.Assert.assertEquals;
@@ -40,7 +39,7 @@ import static org.springframework.test.web.server.setup.MockMvcBuilders.webAppli
 		"classpath:META-INF/shared.xml" }, loader = MockWebApplicationContextLoader.class)
 @MockWebApplication(name = "transport.AccCheckController", webapp = "classpath:.")
 @TransactionConfiguration(transactionManager = "persistence.TransactionManager", defaultRollback = true)
-@Transactional
+//@Transactional
 public class PhoneNumberControllerTestIT {
 	
 	private MockMvc mockMvc;
