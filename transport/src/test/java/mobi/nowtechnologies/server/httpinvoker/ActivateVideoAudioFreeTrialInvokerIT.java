ca94c15bce906eb528a54b52b9ed934b065a288a
Alexander Buzmakoff <Alexander.Buzmakoff@musicqubed.com>
2013-12-02 14:55:43 +0200

    IMP-3211 Added utility to invoke http endpoints basing on file input data

---
+++ transport/src/test/java/mobi/nowtechnologies/server/httpinvoker/ActivateVideoAudioFreeTrialInvokerIT.java
@@ -0,0 +1,82 @@
+package mobi.nowtechnologies.server.httpinvoker;
+
+import mobi.nowtechnologies.server.mock.MockWebApplication;
+import mobi.nowtechnologies.server.mock.MockWebApplicationContextLoader;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.transaction.TransactionConfiguration;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.client.HttpServerErrorException;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import javax.annotation.Resource;
+import java.util.Map;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {
+        "classpath:transport-servlet-test.xml",
+        "classpath:META-INF/service-test.xml",
+        "classpath:META-INF/soap.xml",
+        "classpath:META-INF/dao-test.xml",
+        "classpath:META-INF/soap.xml",
+        "classpath:META-INF/shared.xml"}, loader = MockWebApplicationContextLoader.class)
+@MockWebApplication(name = "transport.ActivateVideoAudioFreeTrialController", webapp = "classpath:.")
+@TransactionConfiguration(transactionManager = "persistence.TransactionManager", defaultRollback = true)
+@Transactional
+public class ActivateVideoAudioFreeTrialInvokerIT {
+    @Autowired
+    private ApplicationContext applicationContext;
+    @Resource
+    private UrlsProducer urlsProducer;
+    @Resource
+    private RestTemplate restTemplate;
+    @Resource
+    private Reporter reporter;
+    @Resource
+    private DataToUrlStrategy videoAudioFreeTrialUrlStrategy;
+
+    @Test
+    @Ignore
+    public void testInvokeRealHttpToActivateVideoAudioFreeTrial() throws Exception {
+        final String target = "localhost";
+
+        org.springframework.core.io.Resource resource = applicationContext.getResource("classpath:testData/users-ids.txt");
+
+        HttpHeaders httpHeaders = createXmlHttpHeaders();
+        HttpEntity<String> request = new HttpEntity<String>(httpHeaders);
+
+        Map<String,String> urlsMap = urlsProducer.produceUrls(resource, videoAudioFreeTrialUrlStrategy);
+        for (Map.Entry<String, String> url : urlsMap.entrySet()) {
+            UriComponentsBuilder builder = UriComponentsBuilder.fromUriString(url.getValue());
+            builder.host(target);
+            builder.port(8080);
+            builder.scheme("http");
+
+            String endpoint = builder.build().toString();
+
+            ResponseEntity<String> response;
+            try {
+                response = restTemplate.postForEntity(endpoint, request, String.class);
+                reporter.report(url.getKey(), response);
+            } catch (HttpServerErrorException e) {
+                reporter.reportError(url.getKey(), e);
+            }
+        }
+    }
+
+    private HttpHeaders createXmlHttpHeaders() {
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.set("Content-Type", "application/x-www-form-urlencoded");
+        httpHeaders.set("Accept", "application/xml");
+        return httpHeaders;
+    }
+}
---
+++ transport/src/test/java/mobi/nowtechnologies/server/httpinvoker/UrlsProducer.java
@@ -0,0 +1,26 @@
+package mobi.nowtechnologies.server.httpinvoker;
+
+import com.google.common.base.Charsets;
+import com.google.common.io.Files;
+import org.springframework.core.io.Resource;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class UrlsProducer {
+    public Map<String, String> produceUrls(Resource resource, DataToUrlStrategy strategy) throws IOException {
+        List<String> lines = Files.readLines(resource.getFile(), Charsets.UTF_8);
+
+        Map<String, String> urls = new HashMap<String, String>();
+
+        for (String line : lines) {
+            String trim = line.trim();
+
+            urls.put(line, strategy.createUrl(trim));
+        }
+
+        return urls;
+    }
+}
