75ecc4fcfe6afd04e9e0d610ca8e62e0f91c4b8f
oleg.artomov@musicqubed.com <JniXxsLu>
2013-12-19 14:16:25 +0200

    Append logging info

--- service/src/main/java/mobi/nowtechnologies/server/service/aop/SMSNotification.java
+++ service/src/main/java/mobi/nowtechnologies/server/service/aop/SMSNotification.java
@@ -59,10 +59,10 @@ public class SMSNotification {
 	@Around("startCreditCardPayment()  || startPayPalPayment() || startO2PSMSPayment() || startMigPayment() || startVFPSMSPayment()")
 	public Object startPayment(ProceedingJoinPoint joinPoint) throws Throwable {
         PendingPayment pendingPayment = (PendingPayment) joinPoint.getArgs()[0];
-        LOGGER.info("start payment", pendingPayment);
+        LOGGER.info("start payment: {}", pendingPayment);
         Object object = joinPoint.proceed();
         userNotificationService.sendPaymentFailSMS(pendingPayment);
-        LOGGER.info("finish payment", pendingPayment);
+        LOGGER.info("finish payment {}", pendingPayment);
 		return object;
 	}
 
