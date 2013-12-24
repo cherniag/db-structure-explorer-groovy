9950e8e2cd0ee5bd105607f75a640bfdb7e4f45b
Gennadii <gennadii.cherniaiev@musicqubed.com>
2013-12-24 14:42:40 +0200

    [IMP-3219] - Send a reminder message for users subscribed for 28 days continuously [VF NZ] * change user_id column definition prior to user.i column

--- persistence/src/main/resources/sql/release5.3-SNAPSHOT.sql
+++ persistence/src/main/resources/sql/release5.3-SNAPSHOT.sql
@@ -4,14 +4,14 @@ CREATE TABLE `auto_opt_in_exempt_ph_number` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
-CREATE TABLE 'tb_tasks' (
-  'taskType' varchar(50) NOT NULL,
-  'id' bigint(20) NOT NULL AUTO_INCREMENT,
-  'creationTimestamp' bigint(20) NOT NULL,
-  'executionTimestamp' bigint(20) NOT NULL,
-  'taskStatus' char(25) DEFAULT NULL,
-  'user_id' int(11) DEFAULT NULL,
-  PRIMARY KEY ('id'),
-  KEY 'tb_tasks_pk' ('user_id'),
-  CONSTRAINT 'tbTasksUserId_tbUsersI_fk' FOREIGN KEY ('user_id') REFERENCES 'tb_users' ('i')
+CREATE TABLE `tb_tasks` (
+  `taskType` varchar(50) NOT NULL,
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `creationTimestamp` bigint(20) NOT NULL,
+  `executionTimestamp` bigint(20) NOT NULL,
+  `taskStatus` char(25) DEFAULT NULL,
+  `user_id` int(10) unsigned DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `tb_tasks_pk` (`user_id`),
+  CONSTRAINT `tbTasksUserId_tbUsersI_fk` FOREIGN KEY (`user_id`) REFERENCES `tb_users` (`i`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
