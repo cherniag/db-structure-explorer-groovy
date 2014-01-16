1337f7af7f78807dee7d70a71e09b46627c9b639
Gennadii <gennadii.cherniaiev@musicqubed.com>
2014-01-16 10:55:58 +0200

    [GO-136] Remove table tb_tasks creation from release5.3.sql

--- persistence/src/main/resources/sql/release5.3-SNAPSHOT.sql
+++ persistence/src/main/resources/sql/release5.3-SNAPSHOT.sql
@@ -2,16 +2,3 @@ CREATE TABLE `auto_opt_in_exempt_ph_number` (
   `phone` varchar(255) NOT NULL,
   PRIMARY KEY (`phone`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-
-CREATE TABLE `tb_tasks` (
-  `taskType` varchar(50) NOT NULL,
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `creationTimestamp` bigint(20) NOT NULL,
-  `executionTimestamp` bigint(20) NOT NULL,
-  `taskStatus` char(25) DEFAULT NULL,
-  `user_id` int(10) unsigned DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `tb_tasks_pk` (`user_id`),
-  CONSTRAINT `tbTasksUserId_tbUsersI_fk` FOREIGN KEY (`user_id`) REFERENCES `tb_users` (`i`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
