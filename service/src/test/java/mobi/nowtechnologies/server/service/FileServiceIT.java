19c360b420d85d619d520f43c9acb01387da6e98
Alexander Kotlyarov <alexander.kotlyarov@musicqubed.com>
2014-02-03 14:56:55 +0200

    GO-268: fixed FileService tests

--- service/src/test/java/mobi/nowtechnologies/server/service/FileServiceIT.java
+++ service/src/test/java/mobi/nowtechnologies/server/service/FileServiceIT.java
@@ -1,9 +1,6 @@
 package mobi.nowtechnologies.server.service;
 
-import mobi.nowtechnologies.server.persistence.dao.PersistenceException;
 import mobi.nowtechnologies.server.persistence.domain.User;
-import mobi.nowtechnologies.server.service.exception.ServiceException;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
@@ -59,93 +56,70 @@ public class FileServiceIT {
 		assertTrue(result.exists());
 	}
 
-	@Test(expected = ServiceException.class)
-    @Ignore
+	@Test(expected = IllegalArgumentException.class)
 	public void testGetFile_WhenImageDoesNotExist()
 		throws Exception {
-		String mediaId = "47";
+		String mediaId = "US-UM7-11-00061";
 		FileService.FileType fileType = FileService.FileType.IMAGE_RESOLUTION;
-		int userId = 1;
 		String fileResolution = "wrong";
 
-		File result = fileService.getFile(mediaId, fileType, fileResolution, user);
+		File result = service.getFile(mediaId, fileType, fileResolution, user);
 		assertNull(result);
 	}
 
 	@Test
-    @Ignore
 	public void testGetFile_WhenMediaFileExists()
 		throws Exception {
-		String mediaId = "47";
+		String mediaId = "US-UM7-11-00061";
 		FileService.FileType fileType = FileService.FileType.AUDIO;
-		int userId = 1;
 		String fileResolution = "fileResolution";
 
-		File result = fileService.getFile(mediaId, fileType, fileResolution, user);
+		File result = service.getFile(mediaId, fileType, fileResolution, user);
 
 		assertNotNull(result);
 		assertTrue(result.exists());
 	}
 
-	@Test(expected = ServiceException.class)
-    @Ignore
+	@Test(expected = IllegalArgumentException.class)
 	public void testGetFile_WhenMediaFileDoesNotExists()
 		throws Exception {
-		String mediaId = "49";
+		String mediaId = "US-UM7-11-00067";
 		FileService.FileType fileType = FileService.FileType.AUDIO;
 		int userId = 1;
 		String fileResolution = "fileResolution";
 
-		File result = fileService.getFile(mediaId, fileType, fileResolution, user);
+		File result = service.getFile(mediaId, fileType, fileResolution, user);
 		assertNull(result);
 	}
 
-	@Test(expected = ServiceException.class)
-    @Ignore
+	@Test(expected = IllegalArgumentException.class)
 	public void testGetFile_WhenMediaIdIsNull()
 		throws Exception {
 		String mediaId = null;
 		FileService.FileType fileType = FileService.FileType.AUDIO;
-		int userId = 1;
 		String fileResolution = "";
 
-		File result = fileService.getFile(mediaId, fileType, fileResolution, user);
+		File result = service.getFile(mediaId, fileType, fileResolution, user);
 	}
 
-	@Test(expected = ServiceException.class)
-    @Ignore
+	@Test(expected = IllegalArgumentException.class)
 	public void testGetFile_WhenFileTypeIsNull()
 		throws Exception {
 		String mediaId = "";
 		FileService.FileType fileType = null;
-		int userId = 1;
 		String fileResolution = "";
 
-		File result = fileService.getFile(mediaId, fileType, fileResolution, user);
+		File result = service.getFile(mediaId, fileType, fileResolution, user);
 	}
 
-	@Test(expected = java.lang.NumberFormatException.class)
-    @Ignore
+	@Test(expected = IllegalArgumentException.class)
 	public void testGetFile_InvalidMediaId()
 		throws Exception {
 		String mediaId = "";
 		FileService.FileType fileType = FileService.FileType.AUDIO;
-		int userId = 1;
 		String fileResolution = "";
 
-		File result = fileService.getFile(mediaId, fileType, fileResolution, user);
+		File result = service
+                .getFile(mediaId, fileType, fileResolution, user);
 	}
-
-	@Test(expected = PersistenceException.class)
-    @Ignore
-	public void testGetFile_WrongUserId()
-		throws Exception {
-		String mediaId = "47";
-		FileService.FileType fileType = FileService.FileType.AUDIO;
-		int userId = -1;
-		String fileResolution = null;
-
-		File result = fileService.getFile(mediaId, fileType, fileResolution, user);
-	}
-
 }
