4adab1eb1f5afbbafdd9b27d65fefbce184dc652
oleg.artomov@musicqubed.com <JniXxsLu>
2014-02-05 11:13:18 +0200

    GO-268: Fix tests

--- persistence/src/test/java/mobi/nowtechnologies/server/persistence/repository/MessageRepositoryTest.java
+++ persistence/src/test/java/mobi/nowtechnologies/server/persistence/repository/MessageRepositoryTest.java
@@ -1,11 +1,9 @@
 package mobi.nowtechnologies.server.persistence.repository;
 
-import junit.framework.Assert;
 import mobi.nowtechnologies.server.persistence.dao.CommunityDao;
 import mobi.nowtechnologies.server.persistence.domain.Community;
 import mobi.nowtechnologies.server.persistence.domain.Message;
 import mobi.nowtechnologies.server.shared.dto.NewsDetailDto.MessageType;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
@@ -14,43 +12,43 @@ import org.springframework.test.context.transaction.TransactionConfiguration;
 
 import javax.annotation.Resource;
 
+import static junit.framework.Assert.assertNotNull;
+import static junit.framework.Assert.assertTrue;
+
 
 /**
  * The class <code>MessageRepositoryTest</code> contains tests for the class <code>{@link MessageRepository}</code>.
  *
- * @generatedBy CodePro at 16.05.12 11:10
  * @author Titov Mykhaylo (titov)
  * @version $Revision: 1.0 $
+ * @generatedBy CodePro at 16.05.12 11:10
  */
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = { "/META-INF/dao-test.xml" })
+@ContextConfiguration(locations = {"/META-INF/dao-test.xml"})
 @TransactionConfiguration(defaultRollback = true)
 public class MessageRepositoryTest {
-	
-	@Resource(name = "messageRepository")
-	private MessageRepository messageRepository;
-	
-	@Test
-	@Ignore
-	public void testCount() {
-		long count = messageRepository.count();
-		System.out.println(count);
-	}
-	
-	@Test
-	@Ignore
-	public void testFindOne() {
-		Message message = messageRepository.findOne(1);
-		Assert.assertNotNull(message);
-	}
-	
-	@Test
-	@Ignore
-	public void findMaxPosition(){
-		Community community = CommunityDao.getCommunity("Now Music");
-		Integer position = messageRepository.findMaxPosition(community, MessageType.NEWS, 0L);
-		Assert.assertNotNull(position);
-	}
-	
-	
+
+    @Resource(name = "messageRepository")
+    private MessageRepository messageRepository;
+
+    @Test
+    public void testCount() {
+        long count = messageRepository.count();
+        assertTrue(count > 0);
+    }
+
+    @Test
+    public void testFindOne() {
+        Message message = messageRepository.findOne(1);
+        assertNotNull(message);
+    }
+
+    @Test
+    public void findMaxPosition() {
+        Community community = CommunityDao.getCommunity("Now Music");
+        Integer position = messageRepository.findMaxPosition(community, MessageType.NEWS, 1315686788000L);
+        assertNotNull(position);
+    }
+
+
 }
