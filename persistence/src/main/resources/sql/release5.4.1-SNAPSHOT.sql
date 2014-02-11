7c57c06802b90880885b960f8f79ab4b9d15e4dc
Alexander Kotlyarov <alexander.kotlyarov@musicqubed.com>
2014-02-11 13:22:33 +0200

    GO-276: added sql

--- persistence/src/main/resources/sql/release5.4.1-SNAPSHOT.sql
+++ persistence/src/main/resources/sql/release5.4.1-SNAPSHOT.sql
@@ -9,11 +9,22 @@ CREATE TABLE `tb_abstractSocialInfo` (
 
 CREATE TABLE `tb_fbUserInfo` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `email` varchar(100) NOT NULL,
-  `firstName` varchar(100),
-  `surname` varchar(100),
-  `profileUrl` varchar(100),
-  `fbId` varchar(100),
-  `userName` varchar(100),
+  `email` varchar(30) NOT NULL,
+  `firstName` varchar(30),
+  `surname` varchar(30),
+  `profileUrl` varchar(200),
+  `fbId` varchar(30),
+  `userName` varchar(300),
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
+
+
+CREATE TABLE `activation_emails` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `activated` bit(1) DEFAULT b'0',
+  `deviceUID` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `token` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `activation_emails_email_deviceUID_token` (`email`,`deviceUID`,`token`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8
