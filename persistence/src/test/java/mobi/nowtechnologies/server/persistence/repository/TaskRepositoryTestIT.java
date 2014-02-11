90db8cfedb0fc0162c13fbac3c87237f63361595
oleg.artomov@musicqubed.com <JniXxsLu>
2014-02-11 11:54:32 +0200

    GO-268: Code cleanup

--- persistence/src/test/java/mobi/nowtechnologies/server/persistence/repository/TaskRepositoryTestIT.java
+++ persistence/src/test/java/mobi/nowtechnologies/server/persistence/repository/TaskRepositoryTestIT.java
@@ -1,42 +1,33 @@
 package mobi.nowtechnologies.server.persistence.repository;
 
+import com.google.common.collect.Iterables;
 import mobi.nowtechnologies.server.persistence.domain.TaskFactory;
 import mobi.nowtechnologies.server.persistence.domain.User;
 import mobi.nowtechnologies.server.persistence.domain.UserFactory;
-import mobi.nowtechnologies.server.persistence.domain.enums.TaskStatus;
 import mobi.nowtechnologies.server.persistence.domain.task.SendChargeNotificationTask;
 import mobi.nowtechnologies.server.persistence.domain.task.Task;
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
-import org.springframework.jdbc.core.RowMapper;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.AbstractTransactionalJUnit4SpringContextTests;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.transaction.TransactionConfiguration;
-import org.springframework.transaction.annotation.Transactional;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import javax.annotation.Resource;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import java.util.List;
 import java.util.UUID;
 
 import static java.lang.System.currentTimeMillis;
 import static org.hamcrest.CoreMatchers.*;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.Assert.assertEquals;
 
 /**
  * User: gch
  * Date: 12/17/13
  */
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = {"/META-INF/dao-test.xml"})
-@TransactionConfiguration(transactionManager = "persistence.TransactionManager", defaultRollback = true)
-@Transactional
-public class TaskRepositoryTestIT extends AbstractTransactionalJUnit4SpringContextTests {
+public class TaskRepositoryTestIT extends AbstractRepositoryIT {
     public static final String SEND_CHARGE_NOTIFICATION_TASK_NAME = SendChargeNotificationTask.class.getSimpleName();
     public static final String WRONG_TASK_NAME = "WRONG";
     public static final int WRONG_USER_ID = 1500;
@@ -46,21 +37,30 @@ public class TaskRepositoryTestIT extends AbstractTransactionalJUnit4SpringConte
     @Autowired
     private UserRepository userRepository;
 
+    @Resource
+    private DrmRepository drmRepository;
+
+    @Resource
+    private SendChargeNotificationTaskRepository repository;
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
     @Before
     public void setUp() {
-        deleteFromTables("tb_tasks");
-        deleteFromTables("tb_drm");
-        deleteFromTables("tb_users");
+        taskRepository.deleteAll();
+        drmRepository.deleteAllInBatch();
+        userRepository.deleteAllInBatch();
     }
 
     @Test
     public void checkSendChargeNotificationTaskIsSavedSuccessfully() {
         SendChargeNotificationTask task = TaskFactory.createSendChargeNotificationTask();
         task.setId(null);
-        taskRepository.save(task);
-        List<SendChargeNotificationTask> savedTasks = simpleJdbcTemplate.query("select * from tb_tasks", new SendChargeNotificationTaskMapper());
+        repository.save(task);
+        List<SendChargeNotificationTask> savedTasks = repository.findAll();
         assertThat(savedTasks.size(), is(1));
-        Task actual = savedTasks.get(0);
+        Task actual = Iterables.getFirst(savedTasks, null);
         assertThat(actual, instanceOf(SendChargeNotificationTask.class));
         assertThat(actual.getId(), is(task.getId()));
         assertThat(actual.getExecutionTimestamp(), is(task.getExecutionTimestamp()));
@@ -131,8 +131,8 @@ public class TaskRepositoryTestIT extends AbstractTransactionalJUnit4SpringConte
         User user2 = createAndSaveUser();
         SendChargeNotificationTask task2 = createAndSaveSendChargeNotificationTask(user2, currentTimeMillis() - 1L);
         SendChargeNotificationTask task3 = createAndSaveSendChargeNotificationTask(user2, currentTimeMillis() + 100 * 1000L);
-        int totalCount = simpleJdbcTemplate.queryForInt("select count(*) from tb_tasks");
-        assertThat(totalCount, is(3));
+        long totalCount = taskRepository.count();
+        assertThat(totalCount, is(3l));
         Pageable pageable = new PageRequest(0, 5);
         List<Task> taskList = taskRepository.findTasksToExecute(currentTimeMillis(), pageable);
         assertThat(taskList.size(), is(2));
@@ -147,8 +147,8 @@ public class TaskRepositoryTestIT extends AbstractTransactionalJUnit4SpringConte
         for (int i = 0; i < 20; i++) {
             createAndSaveSendChargeNotificationTask(user1, currentTimeMillis() - 1L);
         }
-        int totalCount = simpleJdbcTemplate.queryForInt("select count(*) from tb_tasks");
-        assertThat(totalCount, is(20));
+        long totalCount = taskRepository.count();
+        assertThat(totalCount, is(20l));
         Pageable pageable = new PageRequest(0, 8);
         List<Task> taskList = taskRepository.findTasksToExecute(currentTimeMillis(), pageable);
         assertThat(taskList.size(), is(8));
@@ -160,12 +160,14 @@ public class TaskRepositoryTestIT extends AbstractTransactionalJUnit4SpringConte
     @Test
     public void checkTaskIsSavedCorrectly() {
         long executionTimestamp = currentTimeMillis();
-        SendChargeNotificationTask task = createAndSaveSendChargeNotificationTask(null, executionTimestamp);
-        assertThat(task, notNullValue());
-        assertThat(task.getId(), notNullValue());
-        taskRepository.updateExecutionTimestamp(task.getId(), executionTimestamp + 1000L);
-        SendChargeNotificationTask savedTask = simpleJdbcTemplate.queryForObject("select * from tb_tasks t where t.id=" + task.getId(), new SendChargeNotificationTaskMapper());
-        assertThat(savedTask.getExecutionTimestamp(), is(executionTimestamp + 1000L));
+        assertEquals(taskRepository.count(), 0);
+        createAndSaveSendChargeNotificationTask(null, executionTimestamp);
+        Long taskId = repository.findAll().get(0).getId();
+        long newExecutionTimestamp = executionTimestamp + 1000L;
+        taskRepository.updateExecutionTimestamp(taskId, newExecutionTimestamp);
+        entityManager.clear();
+        SendChargeNotificationTask savedTask = (SendChargeNotificationTask)taskRepository.findOne(taskId);
+        assertEquals(newExecutionTimestamp, savedTask.getExecutionTimestamp());
     }
 
     @Test
@@ -192,38 +194,12 @@ public class TaskRepositoryTestIT extends AbstractTransactionalJUnit4SpringConte
         return createAndSaveSendChargeNotificationTask(user, currentTimeMillis());
     }
 
-    private SendChargeNotificationTask createAndSaveSendChargeNotificationTask(User user, long executionTimestamp) {
+    private SendChargeNotificationTask createAndSaveSendChargeNotificationTask(User user, Long executionTimestamp) {
         SendChargeNotificationTask task = TaskFactory.createSendChargeNotificationTask();
         task.setUser(user);
-        task.setId(null);
         task.setExecutionTimestamp(executionTimestamp);
         return (SendChargeNotificationTask) taskRepository.save(task);
     }
 
-    private class SendChargeNotificationTaskMapper implements RowMapper<SendChargeNotificationTask> {
-        @Override
-        public SendChargeNotificationTask mapRow(ResultSet resultSet, int i) throws SQLException {
-            assert "SendChargeNotificationTask".equals(resultSet.getString("taskType"));
-            SendChargeNotificationTask chargeNotificationTask = new SendChargeNotificationTask();
-            if (resultSet.getString("id") != null) {
-                chargeNotificationTask.setId(Long.valueOf(resultSet.getString("id")));
-            }
-            if (resultSet.getString("taskStatus") != null) {
-                chargeNotificationTask.setTaskStatus(TaskStatus.valueOf(resultSet.getString("taskStatus")));
-            }
-            if (resultSet.getString("executionTimestamp") != null) {
-                chargeNotificationTask.setExecutionTimestamp(Long.valueOf(resultSet.getString("executionTimestamp")));
-            }
-            if (resultSet.getString("creationTimestamp") != null) {
-                chargeNotificationTask.setCreationTimestamp(Long.valueOf(resultSet.getString("creationTimestamp")));
-            }
-            if (resultSet.getString("user_id") != null) {
-                User user = new User();
-                user.setId(Integer.valueOf(resultSet.getString("user_id")));
-                chargeNotificationTask.setUser(user);
-            }
-            return chargeNotificationTask;
-        }
-    }
 
 }
