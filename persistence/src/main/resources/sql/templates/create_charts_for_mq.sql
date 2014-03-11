75c1591e1cc477f095dcfbd1ab009ebabf4df3fa
oleg.artomov@musicqubed.com <JniXxsLu>
2014-03-11 16:22:40 +0200

    Rename chart names according discussion

--- persistence/src/main/resources/sql/templates/create_charts_for_mq.sql
+++ persistence/src/main/resources/sql/templates/create_charts_for_mq.sql
@@ -1,13 +1,13 @@
-INSERT INTO tb_charts (name, numTracks, genre, timestamp, numBonusTracks, type) VALUES ('HOT_TRACKS_1 FOR MQ', 0, 1, 1389367950, 0, 'HOT_TRACKS');
-INSERT INTO tb_charts (name, numTracks, genre, timestamp, numBonusTracks, type) VALUES ('HOT_TRACKS_2 FOR MQ', 0, 1, 1389367950, 0, 'FIFTH_CHART');
-INSERT INTO tb_charts (name, numTracks, genre, timestamp, numBonusTracks, type) VALUES ('HOT_TRACKS_3 FOR MQ', 0, 1, 1389367950, 0, 'MQ_PLAYLIST_1');
-INSERT INTO tb_charts (name, numTracks, genre, timestamp, numBonusTracks, type) VALUES ('HOT_TRACKS_4 FOR MQ', 0, 1, 1389367950, 0, 'MQ_PLAYLIST_2');
-INSERT INTO tb_charts (name, numTracks, genre, timestamp, numBonusTracks, type) VALUES ('HOT_TRACKS_5 FOR MQ', 0, 1, 1389367950, 0, 'OTHER_CHART');
-INSERT INTO tb_charts (name, numTracks, genre, timestamp, numBonusTracks, type) VALUES ('HOT_TRACKS_6 FOR MQ', 0, 1, 1389367950, 0, 'FOURTH_CHART');
+INSERT INTO tb_charts (name, numTracks, genre, timestamp, numBonusTracks, type) VALUES ('HOT_TRACKS FOR MQ', 0, 1, UNIX_TIMESTAMP(now()), 0, 'HOT_TRACKS');
+INSERT INTO tb_charts (name, numTracks, genre, timestamp, numBonusTracks, type) VALUES ('FIFTH_CHART FOR MQ', 0, 1, UNIX_TIMESTAMP(now()), 0, 'FIFTH_CHART');
+INSERT INTO tb_charts (name, numTracks, genre, timestamp, numBonusTracks, type) VALUES ('MQ_PLAYLIST_1 FOR MQ', 0, 1, UNIX_TIMESTAMP(now()), 0, 'MQ_PLAYLIST_1');
+INSERT INTO tb_charts (name, numTracks, genre, timestamp, numBonusTracks, type) VALUES ('MQ_PLAYLIST_2 FOR MQ', 0, 1, UNIX_TIMESTAMP(now()), 0, 'MQ_PLAYLIST_2');
+INSERT INTO tb_charts (name, numTracks, genre, timestamp, numBonusTracks, type) VALUES ('OTHER_CHART FOR MQ', 0, 1, UNIX_TIMESTAMP(now()), 0, 'OTHER_CHART');
+INSERT INTO tb_charts (name, numTracks, genre, timestamp, numBonusTracks, type) VALUES ('FOURTH_CHART FOR MQ', 0, 1, UNIX_TIMESTAMP(now()), 0, 'FOURTH_CHART');
 
-INSERT INTO community_charts (chart_id, community_id) VALUES ((select i from tb_charts where name='HOT_TRACKS_1 FOR MQ'), (select id from tb_communities where rewriteURLParameter='mq'));
-INSERT INTO community_charts (chart_id, community_id) VALUES ((select i from tb_charts where name='HOT_TRACKS_2 FOR MQ'), (select id from tb_communities where rewriteURLParameter='mq'));
-INSERT INTO community_charts (chart_id, community_id) VALUES ((select i from tb_charts where name='HOT_TRACKS_3 FOR MQ'), (select id from tb_communities where rewriteURLParameter='mq'));
-INSERT INTO community_charts (chart_id, community_id) VALUES ((select i from tb_charts where name='HOT_TRACKS_4 FOR MQ'), (select id from tb_communities where rewriteURLParameter='mq'));
-INSERT INTO community_charts (chart_id, community_id) VALUES ((select i from tb_charts where name='HOT_TRACKS_5 FOR MQ'), (select id from tb_communities where rewriteURLParameter='mq'));
-INSERT INTO community_charts (chart_id, community_id) VALUES ((select i from tb_charts where name='HOT_TRACKS_6 FOR MQ'), (select id from tb_communities where rewriteURLParameter='mq'));
+INSERT INTO community_charts (chart_id, community_id) VALUES ((select i from tb_charts where name='HOT_TRACKS FOR MQ'), (select id from tb_communities where rewriteURLParameter='mq'));
+INSERT INTO community_charts (chart_id, community_id) VALUES ((select i from tb_charts where name='FIFTH_CHART FOR MQ'), (select id from tb_communities where rewriteURLParameter='mq'));
+INSERT INTO community_charts (chart_id, community_id) VALUES ((select i from tb_charts where name='MQ_PLAYLIST_1 FOR MQ'), (select id from tb_communities where rewriteURLParameter='mq'));
+INSERT INTO community_charts (chart_id, community_id) VALUES ((select i from tb_charts where name='MQ_PLAYLIST_2 FOR MQ'), (select id from tb_communities where rewriteURLParameter='mq'));
+INSERT INTO community_charts (chart_id, community_id) VALUES ((select i from tb_charts where name='OTHER_CHART FOR MQ'), (select id from tb_communities where rewriteURLParameter='mq'));
+INSERT INTO community_charts (chart_id, community_id) VALUES ((select i from tb_charts where name='FOURTH_CHART FOR MQ'), (select id from tb_communities where rewriteURLParameter='mq'));
