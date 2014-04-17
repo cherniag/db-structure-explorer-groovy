fbb4fffb2652941e5f663fa2eca4cd6f922558cb
Mykhailo Titov <mykhayl.titov@musicqubed.com>
2014-04-17 11:56:59 +0300

    GO-1069 - Introduce "promotion" service and integrate with exisiting AUTO_OPT_IN flow

---
+++ persistence/src/main/resources/sql/release5.4.X_O2_PROMO.sql
@@ -0,0 +1,27 @@
+insert into system (release_time_millis, version, release_name) values(unix_timestamp(now()), "release5.4.X_O2_PROMO", "5.4.X_O2_PROMO");
+
+CREATE TABLE `subscription_campaign` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `mobile` varchar(25) NOT NULL,
+  `campaign_id` varchar(25),
+  PRIMARY KEY (`id`),
+  INDEX mobile-campaign_id (mobile, campaign_id)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
+
+-- http://jira.musicqubed.com/browse/GO-1069
+-- Introduce "promotion" service and integrate with exisiting AUTO_OPT_IN flow
+INSERT INTO tb_promotions
+(i , description         , numUsers, maxUsers, startDate , endDate   , isActive, freeWeeks, subWeeks, userGroup, type       , showPromotion, label                  , is_white_listed) VALUES
+(59, 'o2Campaign3G'      , 0       , 0       , 1356342064, 1606780800, true    , 1        , 0       , 10       , 'PromoCode', false        , 'o2Campaign3G'         , false);
+
+INSERT INTO tb_promoCode
+(code                   , promotionId, media_type) VALUES
+('o2Campaign3G'         , LAST_INSERT_ID()         , 'AUDIO');
+
+INSERT INTO tb_promotions
+(i , description         , numUsers, maxUsers, startDate , endDate   , isActive, freeWeeks, subWeeks, userGroup, type       , showPromotion, label                  , is_white_listed) VALUES
+(60, 'o2Campaign4G'      , 0       , 0       , 1356342064, 1606780800, true    , 1        , 0       , 10       , 'PromoCode', false        , 'o2Campaign4G'         , false);
+
+INSERT INTO tb_promoCode
+(code                   , promotionId      , media_type) VALUES
+('o2Campaign4G'         , LAST_INSERT_ID() , 'VIDEO_AND_AUDIO');
