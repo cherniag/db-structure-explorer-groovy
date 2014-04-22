ba6429434105e306f75487cb62a21e0e3f8b9000
Gennadii <gennadii.cherniaiev@musicqubed.com>
2014-04-22 15:21:14 +0300

    change promo_code id selection and remove promotion id hardcoded values

--- persistence/src/main/resources/sql/release5.4.X_O2_PROMO.sql
+++ persistence/src/main/resources/sql/release5.4.X_O2_PROMO.sql
@@ -11,17 +11,17 @@ CREATE TABLE `subscription_campaign` (
 -- http://jira.musicqubed.com/browse/GO-1069
 -- Introduce "promotion" service and integrate with exisiting AUTO_OPT_IN flow
 INSERT INTO tb_promotions
-(i , description         , numUsers, maxUsers, startDate , endDate   , isActive, freeWeeks, subWeeks, userGroup, type       , showPromotion, label                  , is_white_listed) VALUES
-(59, 'o2Campaign3G'      , 0       , 0       , 1356342064, 1606780800, true    , 1        , 0       , 10       , 'PromoCode', false        , 'o2Campaign3G'         , false);
+(description         , numUsers, maxUsers, startDate , endDate   , isActive, freeWeeks, subWeeks, userGroup, type       , showPromotion, label                  , is_white_listed) VALUES
+('o2Campaign3G'      , 0       , 0       , 1356342064, 1606780800, true    , 1        , 0       , 10       , 'PromoCode', false        , 'o2Campaign3G'         , false);
 
 INSERT INTO tb_promoCode
-(code                   , promotionId, media_type) VALUES
-('o2Campaign3G'         , LAST_INSERT_ID()         , 'AUDIO');
+(code                   , promotionId                                                ,  media_type) VALUES
+('o2Campaign3G'         , (select i from tb_promotions where label = 'o2Campaign3G') , 'AUDIO');
 
 INSERT INTO tb_promotions
-(i , description         , numUsers, maxUsers, startDate , endDate   , isActive, freeWeeks, subWeeks, userGroup, type       , showPromotion, label                  , is_white_listed) VALUES
-(60, 'o2Campaign4G'      , 0       , 0       , 1356342064, 1606780800, true    , 1        , 0       , 10       , 'PromoCode', false        , 'o2Campaign4G'         , false);
+(description         , numUsers, maxUsers, startDate , endDate   , isActive, freeWeeks, subWeeks, userGroup, type       , showPromotion, label                  , is_white_listed) VALUES
+('o2Campaign4G'      , 0       , 0       , 1356342064, 1606780800, true    , 1        , 0       , 10       , 'PromoCode', false        , 'o2Campaign4G'         , false);
 
 INSERT INTO tb_promoCode
-(code                   , promotionId      , media_type) VALUES
-('o2Campaign4G'         , LAST_INSERT_ID() , 'VIDEO_AND_AUDIO');
+(code                   , promotionId                                                , media_type) VALUES
+('o2Campaign4G'         , (select i from tb_promotions where label = 'o2Campaign4G') , 'VIDEO_AND_AUDIO');
