21a596aa0a772f470ccebf63f001200cda96a648
oleg.artomov@musicqubed.com <JniXxsLu>
2014-07-02 15:52:11 +0300

    GO-2005: Code cleanup

--- persistence/src/main/resources/sql/release5.4.7-SNAPSHOT.sql
+++
@@ -1,7 +0,0 @@
-insert into system (release_time_millis, version, release_name) values(unix_timestamp(now()), "release5.4.7", "5.4.7");
-
--- http://jira.musicqubed.com/browse/GO-2007
--- [Jadmin] Update search logic to use Track ID under Charts section
-
-ALTER TABLE tb_media MODIFY COLUMN label tinyint(3) unsigned;
-update tb_media set label = null where label = 0;
