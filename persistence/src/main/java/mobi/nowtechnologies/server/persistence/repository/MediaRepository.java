53caf6bb7df92b42e9c26abb99c670c8d751bd27
Mykhailo Titov <mykhayl.titov@musicqubed.com>
2014-07-03 17:11:12 +0300

    GO-2008 - [JADMIN] Use Track ID instead of ISRC under Magazine Channel tab Compilation bug fixed

--- persistence/src/main/java/mobi/nowtechnologies/server/persistence/repository/MediaRepository.java
+++ persistence/src/main/java/mobi/nowtechnologies/server/persistence/repository/MediaRepository.java
@@ -1,6 +1,7 @@
 package mobi.nowtechnologies.server.persistence.repository;
 
 import mobi.nowtechnologies.server.persistence.domain.Media;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
