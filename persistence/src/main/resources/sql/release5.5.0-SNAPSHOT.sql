1e7137ea117b8083cc7ca40203ad88aa56a6e0e6
oleg.artomov@musicqubed.com <JniXxsLu>
2014-08-01 14:42:09 +0300

    GO-2499: Include missed tracks

--- persistence/src/main/resources/sql/release5.5.0-SNAPSHOT.sql
+++ persistence/src/main/resources/sql/release5.5.0-SNAPSHOT.sql
@@ -13,6 +13,71 @@ create table fat_email(
 ALTER TABLE tb_media MODIFY COLUMN label tinyint(3) unsigned;
 update tb_media set label = null where label = 0;
 
+update cn_service.tb_media
+set trackId = 127663 where ISRC = 'USAT21001886';
+
+update cn_service.tb_media
+set trackId = 127653 where ISRC = 'USAT21001269';
+
+update cn_service.tb_media
+set trackId = 43806 where ISRC = 'GBAWV9902021';
+
+update cn_service.tb_media
+set trackId = 23673 where ISRC = 'USAT20904033';
+
+update cn_service.tb_media
+set trackId = 39715 where ISRC = 'GBAWV9902019';
+
+update cn_service.tb_media
+set trackId = 113946 where ISRC = 'USTB10300119';
+
+update cn_service.tb_media
+set trackId = 49271 where ISRC = 'USWB10001880';
+
+update cn_service.tb_media
+set trackId = 56628 where ISRC = 'USWB10905329';
+
+update cn_service.tb_media
+set trackId = 200083 where ISRC = 'USAT20100092';
+
+update cn_service.tb_media
+set trackId = 39708 where ISRC = 'USRE10901161';
+
+update cn_service.tb_media
+set trackId = 39742 where ISRC = 'GBAHT0600164';
+
+update cn_service.tb_media
+set trackId = 54863 where ISRC = 'USWB19903319';
+
+update cn_service.tb_media
+set trackId = 41442 where ISRC = 'USAT20303122';
+
+update cn_service.tb_media
+set trackId = 127654 where ISRC = 'IEABD0100002';
+
+update cn_service.tb_media
+set trackId = 57336 where ISRC = 'GBAAP9000008';
+
+update cn_service.tb_media
+set trackId = 353147 where ISRC = 'VIDEO181021';
+
+update cn_service.tb_media
+set trackId = 127664 where ISRC = 'USAT21001885';
+
+update cn_service.tb_media
+set trackId = 82803 where ISRC = 'GBUM71204768';
+
+update cn_service.tb_media
+set trackId = 813584 where ISRC = 'GBAAP9700206';
+
+update cn_service.tb_media
+set trackId = 681426 where ISRC = 'GBCRL0800322';
+
+update cn_service.tb_media
+set trackId = 353146 where ISRC = 'VIDEO181020';
+
+
+
 alter table tb_media drop index `isrc`;
 
 ALTER TABLE tb_media ADD UNIQUE trackId_idx (trackId);
