d07fa1cdcf271485929ff74e940f62fafe139ef8
Konstantin Tsykulenko <tsykul@gmail.com>
2014-08-18 13:15:03 +0300

    SRV-111. "News" internal ad page type implementation.

---
+++ persistence/src/main/java/mobi/nowtechnologies/server/persistence/domain/streamzine/types/RecognizedPage.java
@@ -0,0 +1,24 @@
+package mobi.nowtechnologies.server.persistence.domain.streamzine.types;
+
+/**
+ * @author kots
+ * @since 8/18/2014.
+ */
+public enum RecognizedPage {
+    GENERIC_NEWS("news");
+
+    private String name;
+
+    public static RecognizedPage recognize(String name) {
+        for (RecognizedPage page: values()) {
+            if (page.name.equals(name)) {
+                return page;
+            }
+        }
+        return null;
+    }
+
+    RecognizedPage(String name) {
+        this.name = name;
+    }
+}
