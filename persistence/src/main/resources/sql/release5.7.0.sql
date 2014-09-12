f2133f924fd338fe000f20b7acf930ce86aca351
Konstantin Tsykulenko <tsykul@gmail.com>
2014-09-12 17:09:46 +0300

    Version updates.

--- persistence/src/main/resources/sql/release5.7.0.sql
+++ persistence/src/main/resources/sql/release5.7.0.sql
@@ -82,3 +82,5 @@ commit;
 
 drop table chart_types;
 alter table sz_deeplink_music_list drop column chart_type;
+
+insert into system (release_time_millis, version, release_name) values(unix_timestamp(now()), "5.7.0", "5.7.0");
