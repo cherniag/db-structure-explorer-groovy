3fbd66ae57f4f500572e4188a14cc2aa99c23df5
Alexander Buzmakoff <Alexander.Buzmakoff@musicqubed.com>
2014-09-12 11:43:14 +0300

    Added missed json/xml annotations for the dto

--- transport/src/main/java/mobi/nowtechnologies/server/dto/transport/ServiceConfigDto.java
+++ transport/src/main/java/mobi/nowtechnologies/server/dto/transport/ServiceConfigDto.java
@@ -1,27 +1,33 @@
 package mobi.nowtechnologies.server.dto.transport;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import mobi.nowtechnologies.server.persistence.domain.versioncheck.VersionCheckStatus;
 
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement(name = "serviceConfig")
 public class ServiceConfigDto {
-    private VersionCheckStatus versionCheckStatus;
+    @XmlElement(name = "status")
+    @JsonProperty(value = "status")
+    private VersionCheckStatus status;
+
+    @XmlElement(name = "message")
+    @JsonProperty(value = "message")
     private String message;
+
+    @XmlElement(name = "link")
+    @JsonProperty(value = "link")
     private String link;
 
-    public ServiceConfigDto(VersionCheckStatus versionCheckStatus, String message, String link) {
-        this.versionCheckStatus = versionCheckStatus;
+    protected ServiceConfigDto() {
+    }
+
+    public ServiceConfigDto(VersionCheckStatus status, String message, String link) {
+        this.status = status;
         this.message = message;
         this.link = link;
     }
-
-    public VersionCheckStatus getVersionCheckStatus() {
-        return versionCheckStatus;
-    }
-
-    public String getMessage() {
-        return message;
-    }
-
-    public String getLink() {
-        return link;
-    }
 }
+
+
