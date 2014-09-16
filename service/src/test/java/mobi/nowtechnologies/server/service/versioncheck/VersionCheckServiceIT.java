e1ed0ac125b8df998ab44adb19366ab9e3028c51
Alexander Buzmakoff <Alexander.Buzmakoff@musicqubed.com>
2014-09-16 11:02:43 +0300

    Moved data preparation from init sql file to the test

--- service/src/test/java/mobi/nowtechnologies/server/service/versioncheck/VersionCheckServiceIT.java
+++ service/src/test/java/mobi/nowtechnologies/server/service/versioncheck/VersionCheckServiceIT.java
@@ -87,13 +87,17 @@ public class VersionCheckServiceIT {
     public void testVersionForO2CommunityWhereRangesArePresent() {
         community = CommunityDao.getCommunity("o2");
         deviceType = DeviceTypeDao.getAndroidDeviceType();
+        checkVersion(0, 0, 1, VersionCheckStatus.REVOKED, "VERSION_REJECTED", "http://play.google.com/new_community_app", O2_TRACKS_APPLICATION_NAME);
+        checkVersion(1, 0, 0, VersionCheckStatus.REVOKED, "VERSION_REJECTED", "http://play.google.com/new_community_app", O2_TRACKS_APPLICATION_NAME);
         checkVersion(1, 4, 0, VersionCheckStatus.REVOKED, "VERSION_REJECTED", "http://play.google.com/new_community_app", O2_TRACKS_APPLICATION_NAME);
+        checkVersion(1, 4, 9, VersionCheckStatus.REVOKED, "VERSION_REJECTED", "http://play.google.com/new_community_app", O2_TRACKS_APPLICATION_NAME);
         checkVersion(1, 5, 0, VersionCheckStatus.REVOKED, "VERSION_REJECTED", "http://play.google.com/new_community_app", O2_TRACKS_APPLICATION_NAME);
         checkVersion(1, 5, 1, VersionCheckStatus.FORCED_UPDATE, "VERSION_FORCED_UPGRADE", "http://play.google.com/new_version_app", O2_TRACKS_APPLICATION_NAME);
         checkVersion(1, 6, 0, VersionCheckStatus.FORCED_UPDATE, "VERSION_FORCED_UPGRADE", "http://play.google.com/new_version_app", O2_TRACKS_APPLICATION_NAME);
         checkVersion(1, 6, 1, VersionCheckStatus.SUGGESTED_UPDATE, "VERSION_SUGGESTED", null, O2_TRACKS_APPLICATION_NAME);
         checkVersion(1, 7, 0, VersionCheckStatus.SUGGESTED_UPDATE, "VERSION_SUGGESTED", null, O2_TRACKS_APPLICATION_NAME);
         checkVersion(1, 7, 1, VersionCheckStatus.CURRENT, null, null, O2_TRACKS_APPLICATION_NAME);
+        checkVersion(2, 0, 1, VersionCheckStatus.CURRENT, null, null, O2_TRACKS_APPLICATION_NAME);
     }
 
 
@@ -121,6 +125,7 @@ public class VersionCheckServiceIT {
         when(clientVersion.major()).thenReturn(major);
         when(clientVersion.minor()).thenReturn(minor);
         when(clientVersion.revision()).thenReturn(revision);
+
         UserAgentRequest userAgentRequest = mock(UserAgentRequest.class);
         when(userAgentRequest.getCommunity()).thenReturn(community);
         when(userAgentRequest.getPlatform()).thenReturn(deviceType);
