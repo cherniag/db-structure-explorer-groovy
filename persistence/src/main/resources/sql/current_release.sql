af39a7a091f853385c7faea71460fa8a866464c7
oleg.artomov@musicqubed.com <JniXxsLu>
2014-10-06 17:16:12 +0300

    SRV-85: Change script

--- persistence/src/main/resources/sql/current_release.sql
+++ persistence/src/main/resources/sql/current_release.sql
@@ -4,7 +4,7 @@ DELETE tb_chartDetail from tb_chartDetail
 LEFT JOIN tb_media  ON tb_chartDetail.media = tb_media.i
 WHERE tb_media.trackId is null;
 
-delete from tb_media tm
+delete from tb_media
 where trackId is null;
 
 ALTER TABLE tb_media MODIFY trackId BIGINT NOT NULL;
