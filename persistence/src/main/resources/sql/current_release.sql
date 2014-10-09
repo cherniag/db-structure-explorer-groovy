6c35d4ddde83172bf54eee08840380794e9a0163
oleg.artomov@musicqubed.com <JniXxsLu>
2014-10-09 10:43:27 +0300

    SRV-85: Make all stuff in one transaction

--- persistence/src/main/resources/sql/current_release.sql
+++ persistence/src/main/resources/sql/current_release.sql
@@ -1,5 +1,8 @@
 -- begin SRV-85
 
+SET autocommit = 0;
+START TRANSACTION;
+
 DELETE tb_chartDetail from tb_chartDetail
 LEFT JOIN tb_media  ON tb_chartDetail.media = tb_media.i
 WHERE tb_media.trackId is null;
@@ -8,5 +11,7 @@ delete from tb_media
 where trackId is null;
 
 ALTER TABLE tb_media MODIFY trackId BIGINT NOT NULL;
+
+commit;
 
 -- end SRV-85
