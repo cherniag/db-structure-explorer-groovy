61904df11537da535b1586a6c7d24fefcaeb96ea
Alexander Buzmakoff <Alexander.Buzmakoff@musicqubed.com>
2014-10-10 12:54:25 +0300

    [SRV-269] Merged

--- persistence/src/main/resources/sql/release5.8.2.sql
+++ persistence/src/main/resources/sql/release5.8.2.sql
@@ -25,4 +25,27 @@ INSERT INTO tb_chartDetail (chart                                , position, med
 commit;
 
 
+-- SRV-269
+SET autocommit = 0;
+START TRANSACTION;
+
+select @community_id:= c.id from tb_communities c where c.name = 'demo4';
+
+INSERT INTO tb_charts (name, numTracks, genre, timestamp            , numBonusTracks, type) VALUES
+  ('HL_UK_PLAYLIST_6 FOR D4' , 0        , 1    , UNIX_TIMESTAMP(now()), 0             , 'HL_UK_PLAYLIST_2'),
+  ('HL_UK_PLAYLIST_7 FOR D4' , 0        , 1    , UNIX_TIMESTAMP(now()), 0             , 'HL_UK_PLAYLIST_2');
+
+INSERT INTO community_charts (chart_id                             , community_id) VALUES
+  ((SELECT i FROM tb_charts WHERE name = 'HL_UK_PLAYLIST_6 FOR D4'), @community_id),
+  ((SELECT i FROM tb_charts WHERE name = 'HL_UK_PLAYLIST_7 FOR D4'), @community_id);
+
+INSERT INTO tb_chartDetail (chart                                , position, media, prevPosition, chgPosition, channel, info, publishTimeMillis         , version, image_filename, image_title              , subtitle                 , title                    , locked, defaultChart) VALUES
+  ((SELECT i FROM tb_charts WHERE name = 'HL_UK_PLAYLIST_6 FOR D4'), 6       , NULL , NULL        , NULL       , NULL   , NULL, UNIX_TIMESTAMP(now())*1000, 1      , NULL          , 'HL_UK_PLAYLIST_6 FOR D4', 'HL_UK_PLAYLIST_6 FOR D4', 'HL_UK_PLAYLIST_6 FOR D4', NULL  , NULL);
+
+INSERT INTO tb_chartDetail (chart                                , position, media, prevPosition, chgPosition, channel, info, publishTimeMillis         , version, image_filename, image_title              , subtitle                 , title                    , locked, defaultChart) VALUES
+  ((SELECT i FROM tb_charts WHERE name = 'HL_UK_PLAYLIST_7 FOR D4'), 7       , NULL , NULL        , NULL       , NULL   , NULL, UNIX_TIMESTAMP(now())*1000, 1      , NULL          , 'HL_UK_PLAYLIST_7 FOR D4', 'HL_UK_PLAYLIST_7 FOR D4', 'HL_UK_PLAYLIST_7 FOR D4', NULL  , NULL);
+
+commit;
+-- SRV-269
+
 
