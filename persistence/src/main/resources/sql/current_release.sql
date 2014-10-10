7c018b8a74a09aa64525b2983e6ee01b8c8e29db
oleg.artomov@musicqubed.com <JniXxsLu>
2014-10-10 17:26:34 +0300

    SRV-285: Fix script

--- persistence/src/main/resources/sql/current_release.sql
+++ persistence/src/main/resources/sql/current_release.sql
@@ -8,6 +8,9 @@ select @mtv1_community_id:= c.id from tb_communities c where c.name = 'mtv1';
 delete from tb_paymentPolicy
 where communityID = @mtv1_community_id and paymentType='iTunesSubscription' and provider = 'GOOGLE_PLUS';
 
+update tb_paymentPolicy
+set provider = null
+where communityID = @mtv1_community_id and paymentType='iTunesSubscription';
 
 COMMIT;
 -- SRV-285
