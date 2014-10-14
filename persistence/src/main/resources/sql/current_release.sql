e8d9b32a3c4827ae7340fd4a256529d240377c68
oleg.artomov@musicqubed.com <JniXxsLu>
2014-10-14 14:56:37 +0300

    SRV-291: Fix price and duration for MTV1

---
+++ persistence/src/main/resources/sql/current_release.sql
@@ -0,0 +1,13 @@
+-- begin SRV-291
+-- SRV-285 [SERVER] Problem with ITunes subscription for MTV
+SET autocommit = 0;
+START TRANSACTION;
+
+select @mtv1_community_id:= c.id from tb_communities c where c.name = 'mtv1';
+
+update tb_paymentPolicy
+set subCost = 4.99, subWeeks = 4
+where communityID = @mtv1_community_id and paymentType in ('iTunesSubscription', 'PAY_PAL');
+
+COMMIT;
+-- end SRV-291
