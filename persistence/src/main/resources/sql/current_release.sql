22fe992940b954b8f97bfff3d0959453817d5cd8
Mykhailo Titov <mykhayl.titov@musicqubed.com>
2014-10-14 15:37:53 +0300

    SRV-290 - MTV1 playlist order fix

--- persistence/src/main/resources/sql/current_release.sql
+++ persistence/src/main/resources/sql/current_release.sql
@@ -5,9 +5,100 @@ UPDATE
       ON cc.chart_id = c.i JOIN tb_communities com
       ON com.id = cc.community_id
 SET
-  cd.POSITION = 0
+  cd.POSITION = 1
 WHERE
   com.name = 'mtv1'
   AND cd.media IS NULL
   AND c.name = 'HOT_TRACKS - MTV1'
+;
+
+UPDATE
+    tb_chartDetail cd JOIN tb_charts c
+      ON c.i = cd.chart JOIN community_charts cc
+      ON cc.chart_id = c.i JOIN tb_communities com
+      ON com.id = cc.community_id
+SET
+  cd.POSITION = 2
+WHERE
+  com.name = 'mtv1'
+  AND cd.media IS NULL
+  AND c.name = 'FIFTH_CHART - MTV1'
+;
+
+UPDATE
+    tb_chartDetail cd JOIN tb_charts c
+      ON c.i = cd.chart JOIN community_charts cc
+      ON cc.chart_id = c.i JOIN tb_communities com
+      ON com.id = cc.community_id
+SET
+  cd.POSITION = 3
+WHERE
+  com.name = 'mtv1'
+  AND cd.media IS NULL
+  AND c.name = 'HL_UK_PLAYLIST_1 - MTV1'
+;
+
+UPDATE
+    tb_chartDetail cd JOIN tb_charts c
+      ON c.i = cd.chart JOIN community_charts cc
+      ON cc.chart_id = c.i JOIN tb_communities com
+      ON com.id = cc.community_id
+SET
+  cd.POSITION = 4
+WHERE
+  com.name = 'mtv1'
+  AND cd.media IS NULL
+  AND c.name = 'HL_UK_PLAYLIST_2 - MTV1'
+;
+
+UPDATE
+    tb_chartDetail cd JOIN tb_charts c
+      ON c.i = cd.chart JOIN community_charts cc
+      ON cc.chart_id = c.i JOIN tb_communities com
+      ON com.id = cc.community_id
+SET
+  cd.POSITION = 5
+WHERE
+  com.name = 'mtv1'
+  AND cd.media IS NULL
+  AND c.name = 'HL_UK_PLAYLIST_3 - MTV1'
+;
+
+UPDATE
+    tb_chartDetail cd JOIN tb_charts c
+      ON c.i = cd.chart JOIN community_charts cc
+      ON cc.chart_id = c.i JOIN tb_communities com
+      ON com.id = cc.community_id
+SET
+  cd.POSITION = 6
+WHERE
+  com.name = 'mtv1'
+  AND cd.media IS NULL
+  AND c.name = 'OTHER_CHART - MTV1'
+;
+
+UPDATE
+    tb_chartDetail cd JOIN tb_charts c
+      ON c.i = cd.chart JOIN community_charts cc
+      ON cc.chart_id = c.i JOIN tb_communities com
+      ON com.id = cc.community_id
+SET
+  cd.POSITION = 7
+WHERE
+  com.name = 'mtv1'
+  AND cd.media IS NULL
+  AND c.name = 'HL_UK_PLAYLIST_4 - MTV1'
+;
+
+UPDATE
+    tb_chartDetail cd JOIN tb_charts c
+      ON c.i = cd.chart JOIN community_charts cc
+      ON cc.chart_id = c.i JOIN tb_communities com
+      ON com.id = cc.community_id
+SET
+  cd.POSITION = 8
+WHERE
+  com.name = 'mtv1'
+  AND cd.media IS NULL
+  AND c.name = 'HL_UK_PLAYLIST_5 - MTV1'
 ;
