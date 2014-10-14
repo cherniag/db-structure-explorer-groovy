d2187af161399dfd5e90141764c9349e739efab5
oleg.artomov@musicqubed.com <JniXxsLu>
2014-10-14 16:38:04 +0300

    SRV-291: Changes according new requirements

--- persistence/src/main/resources/sql/current_release.sql
+++ persistence/src/main/resources/sql/current_release.sql
@@ -5,8 +5,12 @@ START TRANSACTION;
 select @mtv1_community_id:= c.id from tb_communities c where c.name = 'mtv1';
 
 update tb_paymentPolicy
-set subCost = 4.99, subWeeks = 4
+set subCost = 0.99, subWeeks = 1
 where communityID = @mtv1_community_id and paymentType in ('iTunesSubscription', 'PAY_PAL');
+
+update tb_paymentPolicy
+set app_store_product_id = 'com.musicqubed.ios.mtv1.subscription.weekly.0'
+where communityID = @mtv1_community_id and paymentType='iTunesSubscription';
 
 COMMIT;
 -- end SRV-291
