577e28bf5295ce975823cb90d7c0a14c934b0b12
Mykhailo Titov <mykhayl.titov@musicqubed.com>
2014-10-15 17:34:11 +0300

    merged with feature/SRV-296_INVALID_PLAYLIST_POSITION

--- persistence/src/main/resources/sql/release5.8.5.sql
+++ persistence/src/main/resources/sql/release5.8.5.sql
@@ -32,4 +32,87 @@ set app_store_product_id = 'com.musicqubed.ios.mtv1.subscription.weekly.0'
 where communityID = @mtv1_community_id and paymentType='iTunesSubscription';
 
 -- end SRV-291
+
+-- SRV-296
+
+UPDATE
+    tb_chartDetail cd JOIN tb_charts c
+      ON c.i = cd.chart JOIN community_charts cc
+      ON cc.chart_id = c.i JOIN tb_communities com
+      ON com.id = cc.community_id
+SET
+  cd.POSITION = 1
+WHERE
+  com.name = 'demo'
+  AND cd.media IS NULL
+  AND c.name = 'BASIC_CHART FOR DEMO'
+;
+
+UPDATE
+    tb_chartDetail cd JOIN tb_charts c
+      ON c.i = cd.chart JOIN community_charts cc
+      ON cc.chart_id = c.i JOIN tb_communities com
+      ON com.id = cc.community_id
+SET
+  cd.POSITION = 2
+WHERE
+  com.name = 'demo'
+  AND cd.media IS NULL
+  AND c.name = 'HOT_TRACKS FOR DEMO'
+;
+
+UPDATE
+    tb_chartDetail cd JOIN tb_charts c
+      ON c.i = cd.chart JOIN community_charts cc
+      ON cc.chart_id = c.i JOIN tb_communities com
+      ON com.id = cc.community_id
+SET
+  cd.POSITION = 3
+WHERE
+  com.name = 'demo'
+  AND cd.media IS NULL
+  AND c.name = 'FIFTH_CHART FOR DEMO'
+;
+
+UPDATE
+    tb_chartDetail cd JOIN tb_charts c
+      ON c.i = cd.chart JOIN community_charts cc
+      ON cc.chart_id = c.i JOIN tb_communities com
+      ON com.id = cc.community_id
+SET
+  cd.POSITION = 4
+WHERE
+  com.name = 'demo'
+  AND cd.media IS NULL
+  AND c.name = 'HL_UK_PLAYLIST_1 FOR DEMO'
+;
+
+UPDATE
+    tb_chartDetail cd JOIN tb_charts c
+      ON c.i = cd.chart JOIN community_charts cc
+      ON cc.chart_id = c.i JOIN tb_communities com
+      ON com.id = cc.community_id
+SET
+  cd.POSITION = 5
+WHERE
+  com.name = 'demo'
+  AND cd.media IS NULL
+  AND c.name = 'HL_UK_PLAYLIST_2 FOR DEMO'
+;
+
+UPDATE
+    tb_chartDetail cd JOIN tb_charts c
+      ON c.i = cd.chart JOIN community_charts cc
+      ON cc.chart_id = c.i JOIN tb_communities com
+      ON com.id = cc.community_id
+SET
+  cd.POSITION = 6
+WHERE
+  com.name = 'demo'
+  AND cd.media IS NULL
+  AND c.name = 'OTHER_CHART FOR DEMO'
+;
+
+-- SRV-296
+
 COMMIT;
