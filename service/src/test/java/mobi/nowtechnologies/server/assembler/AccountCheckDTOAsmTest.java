bfc84a44f58c8c662b236aadb211dd0d445798d7
Gennadii <gennadii.cherniaiev@musicqubed.com>
2014-10-28 13:54:11 +0200

    [SRV-321] Client apps get email instead of G+ userName data * refactoring

--- service/src/test/java/mobi/nowtechnologies/server/assembler/AccountCheckDTOAsmTest.java
+++ service/src/test/java/mobi/nowtechnologies/server/assembler/AccountCheckDTOAsmTest.java
@@ -1,14 +1,8 @@
 package mobi.nowtechnologies.server.assembler;
 
 import mobi.nowtechnologies.server.persistence.domain.*;
-import mobi.nowtechnologies.server.persistence.domain.social.GooglePlusUserInfo;
 import mobi.nowtechnologies.server.persistence.repository.AutoOptInExemptPhoneNumberRepository;
-import mobi.nowtechnologies.server.persistence.repository.social.GooglePlusUserInfoRepository;
 import mobi.nowtechnologies.server.shared.dto.AccountCheckDTO;
-import mobi.nowtechnologies.server.shared.dto.social.GooglePlusUserDetailsDto;
-import mobi.nowtechnologies.server.shared.dto.social.SocialInfoType;
-import mobi.nowtechnologies.server.shared.enums.Gender;
-import mobi.nowtechnologies.server.shared.enums.ProviderType;
 import mobi.nowtechnologies.server.user.autooptin.AutoOptInRuleService;
 import mobi.nowtechnologies.server.user.rules.RuleResult;
 import mobi.nowtechnologies.server.user.rules.RuleServiceSupport;
@@ -17,8 +11,6 @@ import org.junit.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
-
-import java.text.SimpleDateFormat;
 
 import static junit.framework.Assert.assertEquals;
 import static junit.framework.Assert.assertFalse;
@@ -31,7 +23,7 @@ public class AccountCheckDTOAsmTest {
     @Mock
     private RuleServiceSupport ruleServiceSupport;
     @Mock
-    private GooglePlusUserInfoRepository googlePlusUserInfoRepository;
+    private UserDetailsDtoAsm userDetailsDtoAsm;
 
     private AutoOptInRuleService autoOptInRuleService;
 
@@ -80,7 +72,7 @@ public class AccountCheckDTOAsmTest {
     public void testToAccountCheckDTOWhenUserIsInDatabase() throws Exception {
         when(autoOptInExemptPhoneNumberRepository.findOne(mobile)).thenReturn(autoOptInExemptPhoneNumber);
 
-        AccountCheckDTO dto = accountCheckDTOAsm.toAccountCheckDTO(user, "remember-me-token", null, false, false, false);
+        AccountCheckDTO dto = accountCheckDTOAsm.toAccountCheckDTO(user, "any-remember-me-token", null, false, false, false);
         verify(ruleServiceSupport, times(0)).fireRules(eq(ALL), any(User.class));
 
         assertFalse(dto.subjectToAutoOptIn);
@@ -94,7 +86,7 @@ public class AccountCheckDTOAsmTest {
         boolean isSubjectToAutoOptIn = true;
         when(user.isSubjectToAutoOptIn()).thenReturn(isSubjectToAutoOptIn);
 
-        AccountCheckDTO dto = accountCheckDTOAsm.toAccountCheckDTO(user, "remember-me-token", null, false, false, false);
+        AccountCheckDTO dto = accountCheckDTOAsm.toAccountCheckDTO(user, "any-remember-me-token", null, false, false, false);
 
         verify(user, times(1)).isSubjectToAutoOptIn();
         verify(ruleServiceSupport, times(1)).fireRules(eq(ALL), any(User.class));
@@ -110,7 +102,7 @@ public class AccountCheckDTOAsmTest {
         boolean isSubjectToAutoOptIn = false;
         when(user.isSubjectToAutoOptIn()).thenReturn(isSubjectToAutoOptIn);
 
-        AccountCheckDTO dto = accountCheckDTOAsm.toAccountCheckDTO(user, "remember-me-token", null, false, false, false);
+        AccountCheckDTO dto = accountCheckDTOAsm.toAccountCheckDTO(user, "any-remember-me-token", null, false, false, false);
 
         verify(user, times(0)).isSubjectToAutoOptIn();
         verify(ruleServiceSupport, times(1)).fireRules(eq(ALL), any(User.class));
@@ -119,36 +111,16 @@ public class AccountCheckDTOAsmTest {
     }
 
     @Test
-    public void testToAccCheckDTOForGooglePlusUser() throws Exception {
-        when(user.getProvider()).thenReturn(ProviderType.GOOGLE_PLUS);
-        GooglePlusUserInfo googlePlusUserInfo = mock(GooglePlusUserInfo.class);
-        when(googlePlusUserInfoRepository.findByUser(user)).thenReturn(googlePlusUserInfo);
-        when(googlePlusUserInfo.getEmail()).thenReturn("a@bc.com");
-        when(googlePlusUserInfo.getDisplayName()).thenReturn("Display Name");
-        when(googlePlusUserInfo.getPicture()).thenReturn("http://gp.com/1/1.jpg");
-        when(googlePlusUserInfo.getGooglePlusId()).thenReturn("000001");
-        when(googlePlusUserInfo.getGivenName()).thenReturn("Name");
-        when(googlePlusUserInfo.getFamilyName()).thenReturn("Surname");
-        when(googlePlusUserInfo.getGender()).thenReturn(Gender.FEMALE);
-        when(googlePlusUserInfo.getLocation()).thenReturn("London");
-        when(googlePlusUserInfo.getBirthday()).thenReturn(new SimpleDateFormat("MM/dd/yyyy").parse("07/12/1980"));
-
+    public void testToAccCheckDTOWithDetails() throws Exception {
         when(autoOptInExemptPhoneNumberRepository.findOne(mobile)).thenReturn(null);
-        when(ruleServiceSupport.fireRules(eq(ALL), eq(user))).thenReturn(RuleResult.FAIL_RESULT);
+        when(ruleServiceSupport.fireRules(eq(ALL), any(User.class))).thenReturn(RuleResult.FAIL_RESULT);
 
-        AccountCheckDTO dto = accountCheckDTOAsm.toAccountCheckDTO(user, "remember-me-token", null, false, true, false);
+        boolean isSubjectToAutoOptIn = true;
+        when(user.isSubjectToAutoOptIn()).thenReturn(isSubjectToAutoOptIn);
 
-        //check user details
-        GooglePlusUserDetailsDto dtoUserDetails = (GooglePlusUserDetailsDto) dto.getUserDetails();
-        assertEquals("a@bc.com", dtoUserDetails.getEmail());
-        assertEquals("Display Name", dtoUserDetails.getUserName());
-        assertEquals("http://gp.com/1/1.jpg", dtoUserDetails.getProfileUrl());
-        assertEquals("000001", dtoUserDetails.getGooglePlusId());
-        assertEquals("Name", dtoUserDetails.getFirstName());
-        assertEquals("Surname", dtoUserDetails.getSurname());
-        assertEquals(Gender.FEMALE, dtoUserDetails.getGender());
-        assertEquals("London", dtoUserDetails.getLocation());
-        assertEquals("07/12/1980", dtoUserDetails.getBirthDay());
-        assertEquals(SocialInfoType.GooglePlus, dtoUserDetails.getSocialInfoType());
+        accountCheckDTOAsm.toAccountCheckDTO(user, "any-remember-me-token", null, false, true, false);
+
+        verify(userDetailsDtoAsm, times(1)).toUserDetailsDto(user);
     }
+
 }
