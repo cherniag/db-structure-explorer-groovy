c49ee042d735b311b97fae670cddbfa1878655b5
Maxym Zabuga <Maxym.Zabuga@musicqubed.com>
2014-12-29 14:15:57 +0200

    SRV-473: Commit changes made directly in properties and release script in 5.12.4
    Reviewed by Gena

--- persistence/src/main/resources/sql/release5.12.4.sql
+++ persistence/src/main/resources/sql/release5.12.4.sql
@@ -1,11 +1,11 @@
 insert into system (release_time_millis, version, release_name) values(unix_timestamp(), "5.12.4", "5.12.4");
 
--- SRV-469
+-- SRV-461
 SET autocommit = 0;
 START TRANSACTION;
 /*setup messages*/
 set @last_id_1 = (select count(id) from client_version_messages) + 1;
-insert into client_version_messages(id, message_key, url) values(@last_id_1, 'service.config.mtv.tracks.FORCED_UPDATE.IOS.0.1.0', 'http://preview.mtvtrax.com/');
+insert into client_version_messages(id, message_key, url) values(@last_id_1, 'service.config.mtv.tracks.CURRENT.IOS.0.1.0', null);
 
 set @last_id_2 = (select count(id) from client_version_messages) + 1;
 insert into client_version_messages(id, message_key, url) values(@last_id_2, 'service.config.mtv.tracks.FORCED_UPDATE.IOS.1.0.0', 'https://itunes.apple.com/gb/app/mtv-trax-free-music-player/id925265172');
@@ -19,11 +19,11 @@ set @ios = (select i from tb_deviceTypes where name='IOS');
 set @community_id = (select id from tb_communities where name='mtv1');
 
 insert into client_version_info (device_type_id, community_id, major_number, minor_number, revision_number, qualifier, message_id, status,         application_name)
-                         values ( @ios,         @community_id, 0,            1,            0,               null,     @last_id_1, 'FORCED_UPDATE', 'mtv-tracks'),
+                         values ( @ios,         @community_id, 0,            1,            0,               null,     @last_id_1, 'CURRENT',       'mtv-tracks'),
                                 ( @ios,         @community_id, 1,            0,            0,               null,     @last_id_2, 'FORCED_UPDATE', 'mtv-tracks'),
                                 ( @ios,         @community_id, 1,            0,            2,               null,     @last_id_3, 'FORCED_UPDATE', 'mtv-tracks');
 
 
 COMMIT;
 SET autocommit = 1;
--- End of SRV-469
+-- End of SRV-461
