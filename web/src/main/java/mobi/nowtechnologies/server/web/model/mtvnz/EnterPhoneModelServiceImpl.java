14bc8d0953b8c7023ffd32ac7590b575ef9794e3
Alexander Buzmakoff <Alexander.Buzmakoff@musicqubed.com>
2015-02-20 11:25:08 +0200

    [SRV-533] Added text and title

--- web/src/main/java/mobi/nowtechnologies/server/web/model/mtvnz/EnterPhoneModelServiceImpl.java
+++ web/src/main/java/mobi/nowtechnologies/server/web/model/mtvnz/EnterPhoneModelServiceImpl.java
@@ -8,19 +8,26 @@ import mobi.nowtechnologies.server.service.impl.SmsServiceFacade;
 import mobi.nowtechnologies.server.service.nz.NZSubscriberInfoService;
 import mobi.nowtechnologies.server.service.pincode.PinCodeService;
 import mobi.nowtechnologies.server.service.sms.SMSGatewayService;
+import mobi.nowtechnologies.server.shared.message.CommunityResourceBundleMessageSource;
 import mobi.nowtechnologies.server.web.model.EnterPhoneModelService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.annotation.Resource;
 import java.util.HashMap;
 import java.util.Map;
 
 class EnterPhoneModelServiceImpl implements EnterPhoneModelService {
+    Logger logger = LoggerFactory.getLogger(getClass());
+
     @Resource
     PinCodeService pinCodeService;
     @Resource
     NZSubscriberInfoService nzSubscriberInfoService;
     @Resource
     SmsServiceFacade smsServiceFacade;
+    @Resource
+    CommunityResourceBundleMessageSource communityResourceBundleMessageSource;
 
     @Override
     public Map<String, Object> getModel(User user, String phone) {
@@ -28,15 +35,24 @@ class EnterPhoneModelServiceImpl implements EnterPhoneModelService {
 
         CheckResult checkResult = doCheck(phone);
 
+        logger.info("Result of check {}", checkResult);
+
         if(checkResult.isYes()){
             try {
                 PinCode generated = pinCodeService.generate(user, 4);
+
+                logger.info("Generated Pin Code {}", generated);
 
                 SMSGatewayService smsProvider = smsServiceFacade.getSMSProvider(user.getCommunityRewriteUrl());
 
                 String code = generated.getCode();
 
-                smsProvider.send(phone, "Pin " + code, "The title");
+                String smsText = communityResourceBundleMessageSource.getMessage(user.getCommunityRewriteUrl(), "enter.phone.pin.sms.text", new Object[]{code}, null);
+                String smsTitle = communityResourceBundleMessageSource.getMessage(user.getCommunityRewriteUrl(), "enter.phone.pin.sms.title", null, null);
+
+                smsProvider.send(phone, smsText, smsTitle);
+
+                logger.info("Sms was sent to user id {}", user.getId());
 
                 model.put("result", checkResult);
             } catch (PinCodeException.MaxPinCodesReached maxPinCodesReached) {
