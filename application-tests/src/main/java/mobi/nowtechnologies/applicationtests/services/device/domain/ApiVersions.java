1ef3b4df34f146b5800682db9ab3fc84adcc6773
Eugene Nesterenko <eugene.nesterenko@musicqubed.com>
2015-03-13 16:51:37 +0200

    [SRV-391] Test.

--- application-tests/src/main/java/mobi/nowtechnologies/applicationtests/services/device/domain/ApiVersions.java
+++ application-tests/src/main/java/mobi/nowtechnologies/applicationtests/services/device/domain/ApiVersions.java
@@ -1,8 +1,10 @@
 package mobi.nowtechnologies.applicationtests.services.device.domain;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 import org.springframework.util.Assert;
@@ -17,7 +19,54 @@ public class ApiVersions {
     public static ApiVersions from(Collection<String> versions) {
         ApiVersions apiVersions = new ApiVersions();
         apiVersions.versions.addAll(versions);
-        Collections.sort(apiVersions.versions);
+        Collections.sort(apiVersions.versions, new Comparator<String>() {
+            @Override
+            public int compare(String v1, String v2) {
+                v1 = v1.replaceAll("\\s", "");
+                v2 = v2.replaceAll("\\s", "");
+                String[] a1 = v1.split("\\.");
+                String[] a2 = v2.split("\\.");
+                List<String> l1 = Arrays.asList(a1);
+                List<String> l2 = Arrays.asList(a2);
+
+
+                int i=0;
+                while(true){
+                    Double d1 = null;
+                    Double d2 = null;
+
+                    try{
+                        d1 = Double.parseDouble(l1.get(i));
+                    }catch(IndexOutOfBoundsException e){
+                    }
+
+                    try{
+                        d2 = Double.parseDouble(l2.get(i));
+                    }catch(IndexOutOfBoundsException e){
+                    }
+
+                    if (d1 != null && d2 != null) {
+                        if (d1.doubleValue() > d2.doubleValue()) {
+                            return 1;
+                        } else if (d1.doubleValue() < d2.doubleValue()) {
+                            return -1;
+                        }
+                    } else if (d2 == null && d1 != null) {
+                        if (d1.doubleValue() > 0) {
+                            return 1;
+                        }
+                    } else if (d1 == null && d2 != null) {
+                        if (d2.doubleValue() > 0) {
+                            return -1;
+                        }
+                    } else {
+                        break;
+                    }
+                    i++;
+                }
+                return 0;
+            }
+        });
         return apiVersions;
     }
 
