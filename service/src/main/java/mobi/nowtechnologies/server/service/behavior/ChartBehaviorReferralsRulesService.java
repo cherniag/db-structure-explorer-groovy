067a1e6dda792bf14f9a32ea8184fcb9cb933e5c
Alexander Buzmakoff <Alexander.Buzmakoff@musicqubed.com>
2015-03-13 16:11:41 +0200

    [SRV-645] FREEMIUM rules should not be applied when unlocking playlists in DEFAULT mode by refer friends

--- service/src/main/java/mobi/nowtechnologies/server/service/behavior/ChartBehaviorReferralsRulesService.java
+++ service/src/main/java/mobi/nowtechnologies/server/service/behavior/ChartBehaviorReferralsRulesService.java
@@ -1,16 +1,18 @@
 package mobi.nowtechnologies.server.service.behavior;
 
 import mobi.nowtechnologies.server.persistence.domain.UserStatusType;
+import mobi.nowtechnologies.server.persistence.domain.behavior.BehaviorConfig;
+import mobi.nowtechnologies.server.persistence.domain.behavior.BehaviorConfigType;
 import mobi.nowtechnologies.server.persistence.domain.behavior.ChartBehaviorType;
 
 class ChartBehaviorReferralsRulesService {
 
-    ChartBehaviorType newType(ChartBehaviorInfo info) {
+    ChartBehaviorType newType(BehaviorConfig behaviorConfig, ChartBehaviorInfo info) {
         if (info.userStatusType == UserStatusType.FREE_TRIAL) {
             return ChartBehaviorType.NORMAL;
         }
 
-        if (info.userStatusType == UserStatusType.LIMITED) {
+        if (behaviorConfig.getType().equals(BehaviorConfigType.FREEMIUM) && info.userStatusType == UserStatusType.LIMITED) {
             return ChartBehaviorType.SHUFFLED;
         }
 
