fa40e77ab1e700e8963f23de42f5db3d60ae7007
Alexander Buzmakoff <Alexander.Buzmakoff@musicqubed.com>
2015-03-13 17:01:21 +0200

    [SRV-549] Improved code quality

--- service/src/main/java/mobi/nowtechnologies/server/apptests/VFNZSMSGatewayServiceImplMock.java
+++ service/src/main/java/mobi/nowtechnologies/server/apptests/VFNZSMSGatewayServiceImplMock.java
@@ -1,14 +1,13 @@
 package mobi.nowtechnologies.server.apptests;
 
 import mobi.nowtechnologies.server.persistence.apptests.domain.Email;
-import mobi.nowtechnologies.server.service.sms.SMPPMessage;
 import mobi.nowtechnologies.server.service.sms.SMSResponse;
 import mobi.nowtechnologies.server.service.vodafone.impl.VFNZSMSGatewayServiceImpl;
-import org.jsmpp.bean.SMSCDeliveryReceipt;
-import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+
+import org.springframework.transaction.annotation.Transactional;
 
 public class VFNZSMSGatewayServiceImplMock extends VFNZSMSGatewayServiceImpl {
     @PersistenceContext
@@ -18,20 +17,6 @@ public class VFNZSMSGatewayServiceImplMock extends VFNZSMSGatewayServiceImpl {
     @Transactional
     public SMSResponse send(String numbers, String message, String title) {
         entityManager.persist(new Email("", new String[]{numbers}, title, message));
-        return new SMSResponse() {
-            @Override
-            public boolean isSuccessful() {
-                return true;
-            }
-
-            @Override
-            public String getDescriptionError() {
-                return null;
-            }
-        };
-    }
-
-    public SMSResponse send(String numbers, String message, String originator, SMSCDeliveryReceipt smscDeliveryReceipt, long expireTimeMillis) {
         return new SMSResponse() {
             @Override
             public boolean isSuccessful() {
--- service/src/test/java/mobi/nowtechnologies/server/service/listener/SubBalancePaymentListenerTest.java
+++ service/src/test/java/mobi/nowtechnologies/server/service/listener/SubBalancePaymentListenerTest.java
@@ -183,9 +183,7 @@ public class SubBalancePaymentListenerTest {
 
         fixtureSubBalancePaymentListener = new SubBalancePaymentListener();
         fixtureSubBalancePaymentListener.setPromotionService(mockPromotionService);
-        fixtureSubBalancePaymentListener.setMessageSource(mockCommunityResourceBundleMessageSource);
         fixtureSubBalancePaymentListener.setUserService(mockUserService);
-        fixtureSubBalancePaymentListener.setMigHttpService(mockMigHttpService);
         fixtureSubBalancePaymentListener.setUserNotificationService(mockUserNotificationService);
     }
 }
