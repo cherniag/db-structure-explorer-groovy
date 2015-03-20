fa3466aa59bb1f17c4b22c79cdda902437522213
Mykhailo Titov <mykhayl.titov@musicqubed.com>
2015-03-20 11:52:57 +0200

    SRV-469 [SERVER] Properties should be structured to support. services*.properties cleaned up

--- env/src/test/java/mobi/nowtechnologies/ReloadableMessageSourceIT.java
+++ env/src/test/java/mobi/nowtechnologies/ReloadableMessageSourceIT.java
@@ -15,6 +15,7 @@ import org.springframework.context.support.ReloadableResourceBundleMessageSource
 import org.springframework.core.io.DefaultResourceLoader;
 import org.springframework.core.io.ResourceLoader;
 
+import org.junit.*;
 import org.junit.experimental.theories.*;
 import org.junit.runner.*;
 import static org.junit.Assert.*;
@@ -24,6 +25,7 @@ import static org.hamcrest.core.Is.is;
 // @author Titov Mykhaylo (titov) on 06.01.2015.
 //To run this test in idea IDE please run following command: maven -Dtest=ReloadableMessageSourceIT test -DfailIfNoTests=false
 @RunWith(Theories.class)
+@Ignore
 public class ReloadableMessageSourceIT {
 
     @DataPoints
@@ -42,31 +44,32 @@ public class ReloadableMessageSourceIT {
             {"classpath:services", "classpath:props/prod_jadmin/conf/services", "classpath:env/prod_jadmin/conf/services"},
             {"classpath:services", "classpath:props/prod_trackrepo/conf/services", "classpath:env/prod_trackrepo/conf/services"},
 
-//            //admin
-//            {"classpath:admin/src/main/webapp/i18n/messages", "classpath:props/autotest/conf/i18n/admin/messages", "classpath:env/autotest/conf/i18n/admin/messages"},
-//            {"classpath:admin/src/main/webapp/i18n/messages", "classpath:props/cherry/conf/i18n/admin/messages", "classpath:env/cherry/conf/i18n/admin/messages"},
-//            {"classpath:admin/src/main/webapp/i18n/messages", "classpath:props/cucumber/conf/i18n/admin/messages", "classpath:env/cucumber/conf/i18n/admin/messages"},
-//            {"classpath:admin/src/main/webapp/i18n/messages", "classpath:props/kiwi/conf/i18n/admin/messages", "classpath:env/kiwi/conf/i18n/admin/messages"},
-//            {"classpath:admin/src/main/webapp/i18n/messages", "classpath:props/lime/conf/i18n/admin/messages", "classpath:env/lime/conf/i18n/admin/messages"},
-//            {"classpath:admin/src/main/webapp/i18n/messages", "classpath:props/orange/conf/i18n/admin/messages", "classpath:env/orange/conf/i18n/admin/messages"},
-//            {"classpath:admin/src/main/webapp/i18n/messages", "classpath:props/rage/conf/i18n/admin/messages", "classpath:env/rage/conf/i18n/admin/messages"},
-//            {"classpath:admin/src/main/webapp/i18n/messages", "classpath:props/staging/conf/i18n/admin/messages", "classpath:env/staging/conf/i18n/admin/messages"},
-//            {"classpath:admin/src/main/webapp/i18n/messages", "classpath:props/potato/conf/i18n/admin/messages", "classpath:env/potato/conf/i18n/admin/messages"},
-//            {"classpath:admin/src/main/webapp/i18n/messages", "classpath:props/prod_jadmin/conf/i18n/admin/messages", "classpath:env/prod_jadmin/conf/i18n/admin/messages"},
+            //admin
+            {"classpath:admin/src/main/webapp/i18n/messages", "classpath:props/autotest/conf/i18n/admin/messages", "classpath:env/autotest/conf/i18n/admin/messages"},
+            {"classpath:admin/src/main/webapp/i18n/messages", "classpath:props/cherry/conf/i18n/admin/messages", "classpath:env/cherry/conf/i18n/admin/messages"},
+            {"classpath:admin/src/main/webapp/i18n/messages", "classpath:props/cucumber/conf/i18n/admin/messages", "classpath:env/cucumber/conf/i18n/admin/messages"},
+            {"classpath:admin/src/main/webapp/i18n/messages", "classpath:props/kiwi/conf/i18n/admin/messages", "classpath:env/kiwi/conf/i18n/admin/messages"},
+            {"classpath:admin/src/main/webapp/i18n/messages", "classpath:props/lime/conf/i18n/admin/messages", "classpath:env/lime/conf/i18n/admin/messages"},
+            {"classpath:admin/src/main/webapp/i18n/messages", "classpath:props/orange/conf/i18n/admin/messages", "classpath:env/orange/conf/i18n/admin/messages"},
+            {"classpath:admin/src/main/webapp/i18n/messages", "classpath:props/rage/conf/i18n/admin/messages", "classpath:env/rage/conf/i18n/admin/messages"},
+            {"classpath:admin/src/main/webapp/i18n/messages", "classpath:props/staging/conf/i18n/admin/messages", "classpath:env/staging/conf/i18n/admin/messages"},
+            {"classpath:admin/src/main/webapp/i18n/messages", "classpath:props/potato/conf/i18n/admin/messages", "classpath:env/potato/conf/i18n/admin/messages"},
+            {"classpath:admin/src/main/webapp/i18n/messages", "classpath:props/prod_jadmin/conf/i18n/admin/messages", "classpath:env/prod_jadmin/conf/i18n/admin/messages"},
 
-//            //web
-//            {"classpath:web/src/main/webapp/i18n/messages", "classpath:props/autotest/conf/i18n/web/messages", "classpath:env/autotest/conf/i18n/web/messages"},
-//            {"classpath:web/src/main/webapp/i18n/messages", "classpath:props/cherry/conf/i18n/web/messages", "classpath:env/cherry/conf/i18n/web/messages"},
-//            {"classpath:web/src/main/webapp/i18n/messages", "classpath:props/cucumber/conf/i18n/web/messages", "classpath:env/cucumber/conf/i18n/web/messages"},
-//            {"classpath:web/src/main/webapp/i18n/messages", "classpath:props/kiwi/conf/i18n/web/messages", "classpath:env/kiwi/conf/i18n/web/messages"},
-//            {"classpath:web/src/main/webapp/i18n/messages", "classpath:props/lime/conf/i18n/web/messages", "classpath:env/lime/conf/i18n/web/messages"},
-//            {"classpath:web/src/main/webapp/i18n/messages", "classpath:props/orange/conf/i18n/web/messages", "classpath:env/orange/conf/i18n/web/messages"},
-//            {"classpath:web/src/main/webapp/i18n/messages", "classpath:props/rage/conf/i18n/web/messages", "classpath:env/rage/conf/i18n/web/messages"},
-//            {"classpath:web/src/main/webapp/i18n/messages", "classpath:props/staging/conf/i18n/web/messages", "classpath:env/staging/conf/i18n/web/messages"},
-//            {"classpath:web/src/main/webapp/i18n/messages", "classpath:props/potato/conf/i18n/web/messages", "classpath:env/potato/conf/i18n/web/messages"},
-//            {"classpath:web/src/main/webapp/i18n/messages", "classpath:props/prod_db1/conf/i18n/web/messages", "classpath:env/prod_db1/conf/i18n/web/messages"},
-//            {"classpath:web/src/main/webapp/i18n/messages", "classpath:props/prod_db2/conf/i18n/web/messages", "classpath:env/prod_db2/conf/i18n/web/messages"},
+            //web
+            {"classpath:web/src/main/webapp/i18n/messages", "classpath:props/autotest/conf/i18n/web/messages", "classpath:env/autotest/conf/i18n/web/messages"},
+            {"classpath:web/src/main/webapp/i18n/messages", "classpath:props/cherry/conf/i18n/web/messages", "classpath:env/cherry/conf/i18n/web/messages"},
+            {"classpath:web/src/main/webapp/i18n/messages", "classpath:props/cucumber/conf/i18n/web/messages", "classpath:env/cucumber/conf/i18n/web/messages"},
+            {"classpath:web/src/main/webapp/i18n/messages", "classpath:props/kiwi/conf/i18n/web/messages", "classpath:env/kiwi/conf/i18n/web/messages"},
+            {"classpath:web/src/main/webapp/i18n/messages", "classpath:props/lime/conf/i18n/web/messages", "classpath:env/lime/conf/i18n/web/messages"},
+            {"classpath:web/src/main/webapp/i18n/messages", "classpath:props/orange/conf/i18n/web/messages", "classpath:env/orange/conf/i18n/web/messages"},
+            {"classpath:web/src/main/webapp/i18n/messages", "classpath:props/rage/conf/i18n/web/messages", "classpath:env/rage/conf/i18n/web/messages"},
+            {"classpath:web/src/main/webapp/i18n/messages", "classpath:props/staging/conf/i18n/web/messages", "classpath:env/staging/conf/i18n/web/messages"},
+            {"classpath:web/src/main/webapp/i18n/messages", "classpath:props/potato/conf/i18n/web/messages", "classpath:env/potato/conf/i18n/web/messages"},
+            {"classpath:web/src/main/webapp/i18n/messages", "classpath:props/prod_db1/conf/i18n/web/messages", "classpath:env/prod_db1/conf/i18n/web/messages"},
+            {"classpath:web/src/main/webapp/i18n/messages", "classpath:props/prod_db2/conf/i18n/web/messages", "classpath:env/prod_db2/conf/i18n/web/messages"},
         };
+
     @DataPoints
     public static String[] communities = {"o2", "vf_nz", "demo", "mtvnz", "hl_uk", "mtv1", "demo", "demo2", "demo3", "demo4", "demo5", "demo6"};
     @DataPoints
