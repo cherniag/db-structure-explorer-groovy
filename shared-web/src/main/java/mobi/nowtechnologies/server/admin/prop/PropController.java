52eb47ad10a63eb1b83ac67074736062c2649228
Mykhailo Titov <mykhayl.titov@musicqubed.com>
2015-04-07 11:41:59 +0300

    SRV-471 [TECH] Provide the option to inspect all properties server JVM are running

--- shared-web/src/main/java/mobi/nowtechnologies/server/admin/prop/PropController.java
+++ shared-web/src/main/java/mobi/nowtechnologies/server/admin/prop/PropController.java
@@ -8,6 +8,7 @@ import mobi.nowtechnologies.server.shared.message.CommunityResourceBundleMessage
 import mobi.nowtechnologies.server.utils.AppPropResourceConfigurer;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
 
 import java.io.IOException;
 import java.util.Locale;
@@ -17,7 +18,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.support.RequestContextUtils;
 
 // @author Titov Mykhaylo (titov) on 02.04.2015.
 @Controller
@@ -25,13 +28,27 @@ import org.springframework.web.servlet.ModelAndView;
 public class PropController {
 
     @Resource
-    private AppPropResourceConfigurer appPropResourceConfigurer;
+    private CommunityResourceBundleMessageSource messageProps;
 
     @Resource
+    private AppPropResourceConfigurer appPropResourceConfigurer;
+
+    @Resource(name = "serviceMessageSource")
     private CommunityResourceBundleMessageSource communityResourceBundleMessageSource;
 
+    @RequestMapping(value = "/messageProps", method = RequestMethod.GET)
+    public ModelAndView getMessageProps(HttpServletRequest request){
+
+        LocaleResolver localeResolver = RequestContextUtils.getLocaleResolver(request);
+        final Locale locale = localeResolver.resolveLocale(request);
+
+        final Properties properties = messageProps.getProperties(null, locale);
+
+        return new ModelAndView("prop", "properties", properties);
+    }
+
     @RequestMapping(value = "/serviceProps", method = RequestMethod.GET)
-    public ModelAndView getProps(@RequestParam String communityUrl, @RequestParam(required = false) String language, @RequestParam(required = false) String country){
+    public ModelAndView getServiceProps(@RequestParam String communityUrl, @RequestParam(required = false) String language, @RequestParam(required = false) String country){
 
         Locale locale = CommunityResourceBundleMessageSource.DEFAULT_LOCALE;
         if(language != null){
@@ -48,7 +65,7 @@ public class PropController {
     }
 
     @RequestMapping(value = "/appProps", method = RequestMethod.GET)
-    public ModelAndView getProps() throws IOException {
+    public ModelAndView getAppProps() throws IOException {
 
         Properties properties = appPropResourceConfigurer.mergeProperties();
 
