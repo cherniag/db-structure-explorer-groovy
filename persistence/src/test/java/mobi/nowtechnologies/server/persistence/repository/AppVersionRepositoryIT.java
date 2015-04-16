d20a8e829fa4f77884e35efbb7b87b293e15d9fc
Anton Zemliankin <anz@ciklum.com>
2015-04-16 12:38:59 +0300

    [SRV-731] [TECH] Migrate @Deprecated DAOs to Spring Data Repositories. Fix AppVersion Id type

--- persistence/src/test/java/mobi/nowtechnologies/server/persistence/repository/AppVersionRepositoryIT.java
+++ persistence/src/test/java/mobi/nowtechnologies/server/persistence/repository/AppVersionRepositoryIT.java
@@ -4,14 +4,7 @@
 
 package mobi.nowtechnologies.server.persistence.repository;
 
-import mobi.nowtechnologies.server.persistence.domain.AppVersion;
-import mobi.nowtechnologies.server.persistence.domain.Country;
-
 import javax.annotation.Resource;
-
-import java.util.HashSet;
-
-import com.google.common.collect.Sets;
 
 import org.junit.*;
 import static org.junit.Assert.*;
@@ -22,30 +15,11 @@ public class AppVersionRepositoryIT extends AbstractRepositoryIT {
     @Resource
     AppVersionRepository appVersionRepository;
 
-    @Resource
-    CountryRepository countryRepository;
-
     @Test
     public void testGetByCommunityName(){
-        Country country = new Country("UK", "United Kingdom");
-        countryRepository.save(country);
-
-        appVersionRepository.save(getAppVersion("1.1", Sets.newHashSet(country)));
-
-        long versions = appVersionRepository.countAppVersionLinkedWithCountry("1.1", "UK");
+        long versions = appVersionRepository.countAppVersionLinkedWithCountry("CBEMA", "UA");
         assertEquals(1L, versions);
-
-        appVersionRepository.save(getAppVersion("1.1", Sets.newHashSet(country)));
-
-        versions = appVersionRepository.countAppVersionLinkedWithCountry("1.1", "UK");
-        assertEquals(2L, versions);
     }
 
-    private AppVersion getAppVersion(String version, HashSet<Country> countries) {
-        AppVersion appVersion = new AppVersion();
-        appVersion.setName(version);
-        appVersion.setCountries(countries);
-        return appVersion;
-    }
 }
 
