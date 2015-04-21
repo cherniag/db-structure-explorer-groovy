04cac7e33095945773b37cbb26cfd45bce9ee654
Anton Zemliankin <anz@ciklum.com>
2015-04-21 10:40:08 +0300

    [SRV-732] [SERVER] Allow mtv1 user to pay for 'number of track plays' among with the standard subscription options. Payment policy setup changes

--- database/changelog/cn_service/SRV-732/upd_tb_paymentPolicy.sql
+++ database/changelog/cn_service/SRV-732/upd_tb_paymentPolicy.sql
@@ -7,7 +7,7 @@ SET @maxOrder = 10 + (SELECT MAX(payment_order) FROM tb_paymentPolicy WHERE comm
 -- add new payment policies
 insert into tb_paymentPolicy
 ( communityID, subWeeks, subCost,          paymentType, payment_policy_type, shortCode, currencyIso,                             app_store_product_id, advanced_payment_seconds, after_next_sub_payment_seconds, online, duration, duration_unit, payment_order) VALUES
-(@communityId,        0,    1.49, 'iTunesSubscription',         'RECURRENT',        '',       'GBP',                 'com.musicqubed.ios.mtv1.payg.1',                        0,                              0,   true,      120,      'MONTHS',            10);
+(@communityId,        0,    0.79, 'iTunesSubscription',           'ONETIME',        '',       'GBP',                 'com.musicqubed.ios.mtv1.payg.0',                        0,                              0,   true,      100,      'MONTHS',     @maxOrder);
 
 COMMIT;
 SET AUTOCOMMIT = 1;
