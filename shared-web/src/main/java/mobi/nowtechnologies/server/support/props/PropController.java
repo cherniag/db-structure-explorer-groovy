3be31d4db45265519cfb213b8eaf5a1287f793c8
Maxym Zabuga <Maxym.Zabuga@musicqubed.com>
2015-05-05 17:54:37 +0300

    simplify reflection-based access

--- shared-web/src/main/java/mobi/nowtechnologies/server/support/props/PropController.java
+++ shared-web/src/main/java/mobi/nowtechnologies/server/support/props/PropController.java
@@ -8,15 +8,12 @@ import mobi.nowtechnologies.common.util.LocaleUtils;
 
 import javax.annotation.Resource;
 
-import java.lang.reflect.Method;
 import java.util.Locale;
 
 import org.springframework.beans.factory.config.PropertyPlaceholderConfigurer;
-import org.springframework.context.support.MessageSourceSupport;
+import org.springframework.beans.support.ArgumentConvertingMethodInvoker;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
-import org.springframework.core.io.support.PropertiesLoaderSupport;
 import org.springframework.stereotype.Controller;
-import org.springframework.util.ReflectionUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -28,10 +25,10 @@ import org.springframework.web.servlet.ModelAndView;
 public class PropController {
 
     @Resource
-    private MessageSourceSupport messageSource;
+    private PropertyPlaceholderConfigurer propertyPlaceholderConfigurer;
 
     @Resource
-    private PropertyPlaceholderConfigurer propertyPlaceholderConfigurer;
+    private ReloadableResourceBundleMessageSource messageSource;
 
     @Resource
     private ReloadableResourceBundleMessageSource serviceReloadableResourceBundleMessageSource;
@@ -40,47 +37,39 @@ public class PropController {
         this.propertyPlaceholderConfigurer = propertyPlaceholderConfigurer;
     }
 
+    @RequestMapping(value = "/appProps", method = RequestMethod.GET)
+    public ModelAndView getApplicationProperties() throws Exception {
+        Object properties = getValue(propertyPlaceholderConfigurer, "mergeProperties");
+        return new ModelAndView("prop", "properties", properties);
+    }
+
     @RequestMapping(value = "/messageProps", method = RequestMethod.GET)
-    public ModelAndView getMessageProps(@RequestParam(required = false) String community, @RequestParam(required = false) String language, @RequestParam(required = false) String country)
+    public ModelAndView getMessageProperties(@RequestParam(required = false) String community, @RequestParam(required = false) String language, @RequestParam(required = false) String country)
         throws Exception {
-        return internal(community, language, country, messageSource);
+        return internal(LocaleUtils.buildLocale(community, language, country), messageSource);
     }
 
     @RequestMapping(value = "/serviceProps", method = RequestMethod.GET)
-    public ModelAndView getServiceProps(@RequestParam(required = false) String community, @RequestParam(required = false) String language, @RequestParam(required = false) String country)
+    public ModelAndView getServiceProperties(@RequestParam(required = false) String community, @RequestParam(required = false) String language, @RequestParam(required = false) String country)
         throws Exception {
-        return internal(community, language, country, serviceReloadableResourceBundleMessageSource);
+        return internal(LocaleUtils.buildLocale(community, language, country), serviceReloadableResourceBundleMessageSource);
     }
 
-    ModelAndView internal(String community, String language, String country, MessageSourceSupport messageSource) throws Exception {
-        Locale communityLocale = LocaleUtils.buildLocale(community, language, country);
-        final Method getMergedPropertiesMethod = ReloadableResourceBundleMessageSource.class.getDeclaredMethod("getMergedProperties", Locale.class);
-        getMergedPropertiesMethod.setAccessible(true);
-        getMergedPropertiesMethod.setAccessible(true);
+    ModelAndView internal(Locale communityLocale, ReloadableResourceBundleMessageSource messageSource) throws Exception {
+        Object propertiesHolder = getValue(messageSource, "getMergedProperties", communityLocale);
+        Object properties = getValue(propertiesHolder, "getProperties");
 
-        final Object propertiesHolder = ReflectionUtils.invokeMethod(getMergedPropertiesMethod, messageSource, communityLocale);
-
-        Class<?> propertiesHolderClass = Class.forName("org.springframework.context.support.ReloadableResourceBundleMessageSource$PropertiesHolder");
-        final Method getPropertiesMethod = ReflectionUtils.findMethod(propertiesHolderClass, "getProperties");
-
-        final Object properties = ReflectionUtils.invokeMethod(getPropertiesMethod, propertiesHolder);
-
-        ModelAndView modelAndView = new ModelAndView("prop");
-        modelAndView.addObject("properties", properties);
+        ModelAndView modelAndView = new ModelAndView("prop", "properties", properties);
         modelAndView.addObject("locale", communityLocale);
-
         return modelAndView;
     }
 
-
-    @RequestMapping(value = "/appProps", method = RequestMethod.GET)
-    public ModelAndView getAppProps() throws Exception {
-
-        final Method mergePropertiesMethod = PropertiesLoaderSupport.class.getDeclaredMethod("mergeProperties");
-        mergePropertiesMethod.setAccessible(true);
-        final Object mergedProperties = ReflectionUtils.invokeMethod(mergePropertiesMethod, propertyPlaceholderConfigurer);
-
-        return new ModelAndView("prop", "properties", mergedProperties);
+    private Object getValue(Object targetObject, String targetMethod, Object... arguments) throws Exception {
+        ArgumentConvertingMethodInvoker invoker = new ArgumentConvertingMethodInvoker();
+        invoker.setTargetObject(targetObject);
+        invoker.setTargetMethod(targetMethod);
+        invoker.setArguments(arguments);
+        return invoker.invoke();
     }
 
 }
