dded7d564ae68fb7d6ee9dd5d48818bf95668fed
Anton Zemliankin <anz@ciklum.com>
2015-05-08 13:25:01 +0300

    [SRV-662] [TECH] Clean up offer tables. Fix table names

--- database/changelog/cn_service/SRV-662/alt_tab_offers.sql
+++ database/changelog/cn_service/SRV-662/alt_tab_offers.sql
@@ -1,10 +1,10 @@
-alter table tb_pendingpayments drop column offerId;
+alter table tb_pendingPayments drop column offerId;
 
-alter table tb_submittedpayments drop column offerId;
+alter table tb_submittedPayments drop column offerId;
 
-alter table tb_accountlog drop FOREIGN KEY tb_accountLog_U_offerId;
-alter table tb_accountlog drop INDEX tb_accountLog_PK_offerId;
-alter table tb_accountlog drop column offerId;
+alter table tb_accountLog drop FOREIGN KEY tb_accountLog_U_offerId;
+alter table tb_accountLog drop INDEX tb_accountLog_PK_offerId;
+alter table tb_accountLog drop column offerId;
 
 drop table offers_filters;
 drop table offer_items;
