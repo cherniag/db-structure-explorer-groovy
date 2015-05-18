6058a11c9697409571bb926f24ea6816de5d5704
Maxym Zabuga <Maxym.Zabuga@musicqubed.com>
2015-05-18 10:53:39 +0300

    updated deploy.sql script (minor changes)

--- database/changelog/#archive/cn_service/5.20/deploy.sql
+++ database/changelog/#archive/cn_service/5.20/deploy.sql
@@ -1,4 +1,7 @@
+use cn_service;
+
 -- SRV-670
 drop table if exists google_plus_user_info, facebook_user_info, social_info;
 
+-- SRV-755
 insert into system (release_time_millis, version, release_name) values(unix_timestamp(), "5.20", "5.20");
